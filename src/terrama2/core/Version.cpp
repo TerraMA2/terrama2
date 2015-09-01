bd3cf45efea403b2b078af23750920e7a711d96b
edelatin <evandro.delatin@gmail.com>
2015-09-01 11:12:17 -0300

    Fixing Merge Conflicts

--- src/terrama2/core/Version.cpp
+++ src/terrama2/core/Version.cpp
@@ -34,7 +34,7 @@
 // STL
 #include <cassert>
 
-/*
+
 int terrama2::core::Version::majorNumber()
 {
   return TERRAMA2_VERSION_MAJOR;
@@ -73,4 +73,3 @@ int terrama2::core::Version::asInt()
   return TERRAMA2_VERSION;
 }
 
-*/
