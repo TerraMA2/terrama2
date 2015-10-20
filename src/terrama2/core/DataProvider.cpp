e4beed32d5342c8b24d9976207ee162c900fa803
pauloremoli <paulo002@gmail.com>
2015-10-20 09:38:28 -0200

    Adding parameter name to Dataset and DataProvider constructors

--- src/terrama2/core/DataProvider.cpp
+++ src/terrama2/core/DataProvider.cpp
@@ -33,8 +33,9 @@
 // TerraMA2
 #include "DataProvider.hpp"
 
-terrama2::core::DataProvider::DataProvider(const uint64_t id, Kind k)
+terrama2::core::DataProvider::DataProvider(const std::string& name, Kind k, const uint64_t id)
   : id_(id),
+    name_(name),
     kind_(k),
     status_(INACTIVE)
 {
