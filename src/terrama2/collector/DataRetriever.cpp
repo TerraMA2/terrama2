5a1e412719c2c5d4fdd9a3ca3f58923c93296091
Vinicius <vinicampa@gmail.com>
2016-01-08 11:55:31 -0200

    Retrieving the file names

--- src/terrama2/collector/DataRetriever.cpp
+++ src/terrama2/collector/DataRetriever.cpp
@@ -27,8 +27,20 @@
   \author Jano Simas
 */
 
+// LibCurl
+#include <curl/curl.h>
+#include "CurlOpener.hpp"
+
+// Boost
+#include <boost/algorithm/string.hpp>
+
+// QT
+#include <QTranslator>
+
 // TerraMA2
+#include "DataFilter.hpp"
 #include "DataRetriever.hpp"
+#include "Exception.hpp"
 
 
 terrama2::collector::DataRetriever::DataRetriever(const terrama2::core::DataProvider& dataprovider)
@@ -37,9 +49,81 @@ terrama2::collector::DataRetriever::DataRetriever(const terrama2::core::DataProv
   dataprovider_ = dataprovider;
 }
 
-std::string terrama2::collector::DataRetriever::retrieveData(const terrama2::core::DataSetItem& /*datasetitem*/, DataFilterPtr /*filter*/, std::vector<std::string>& /*log_uris*/)
+size_t data_vector(void *ptr, size_t size, size_t nmemb, void *data)
 {
-  return dataprovider_.uri();
+  size_t sizeRead = size * nmemb;
+
+  std::string* block = (std::string*) data;
+  block->append((char *)ptr, sizeRead);
+
+  return sizeRead;
+}
+
+std::string terrama2::collector::DataRetriever::retrieveData(const terrama2::core::DataSetItem& datasetitem, DataFilterPtr filter, std::vector<std::string>& log_uris)
+{
+  std::string uriInput;
+  CURLcode status;
+  std::vector<std::string> vectorFiles;
+  std::string block;
+
+  CurlOpener curl;
+
+  curl.init();
+
+  try
+  {
+    // Get a file listing from server
+    if(curl.fcurl())
+    {
+      uriInput = dataprovider_.uri() + datasetitem.path();
+      // The address of the source
+      curl_easy_setopt(curl.fcurl(), CURLOPT_URL, uriInput.c_str());
+      // List files and directories
+      curl_easy_setopt(curl.fcurl(), CURLOPT_DIRLISTONLY, 1);
+      // Get data to be written in vector
+      curl_easy_setopt(curl.fcurl(), CURLOPT_WRITEFUNCTION, data_vector);
+      // Set a pointer to our block data
+      curl_easy_setopt(curl.fcurl(), CURLOPT_WRITEDATA, (void *)&block);
+      // performs the configurations of curl_easy_setop
+      status = curl_easy_perform(curl.fcurl());
+
+      if (status == CURLE_OK)
+      {
+        boost::split(vectorFiles, block, boost::is_any_of("\n"));
+
+        if(vectorFiles.size() && vectorFiles.back().empty())
+          vectorFiles.pop_back();
+      }
+      else
+      {
+        QString messageError = QObject::tr("Could not list the source files. \n\n Details: \n");
+        messageError.append(curl_easy_strerror(status));
+        throw DataRetrieverFTPException() << ErrorDescription(messageError);
+      }
+
+      // filter file names.
+      std::vector<std::string> vectorNames = filter->filterNames(vectorFiles);
+
+      for( std::string file : vectorNames )
+      {
+        log_uris.push_back(uriInput + file);
+      }
+    }
+  }
+  catch(const std::exception& e)
+  {
+    QString messageError = QObject::tr("Could not check files! \n\n Details: \n");
+    messageError.append(e.what());
+
+    throw DataRetrieverException() << ErrorDescription(messageError);
+  }
+  catch(...)
+  {
+    throw DataRetrieverException() << ErrorDescription(QObject::tr("Unknown Error, Could not check files!"));
+  }
+
+  // returns the absolute path of the folder that contains the files
+  return uriInput;
 }
 
 void terrama2::collector::DataRetriever::open()
