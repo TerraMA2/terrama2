e4b213eed2d964848e833d941932621d9056227c
raphaelrpl <raphael.wcosta@gmail.com>
2016-01-29 11:10:56 -0200

    adding modals: wfsLayer and collectRule for pcd page

--- webapp/controllers/configuration/dataSetPcd.js
+++ webapp/controllers/configuration/dataSetPcd.js
@@ -2,7 +2,17 @@ module.exports = function(app)
 {
     function dataSetPcdController(request, response)
     {
-        response.render("configuration/dataSetPcd");
+        var dataSetType = request.query.type;
+
+        if (dataSetType != "static" && dataSetType != "dynamic")
+        {
+            response.statusCode = 404;
+            response.send('404. Not found');
+        }
+        else
+        {
+            response.render('configuration/dataSetPcd', {kind: "PCD", state: dataSetType});
+        }
     }
 
     return dataSetPcdController;
