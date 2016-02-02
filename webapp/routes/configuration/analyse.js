8ef88003b7ba85f6f5b4c2cfeb1eec6289b32c26
raphaelrpl <raphael.wcosta@gmail.com>
2016-02-02 10:53:19 -0200

    adding analysis page

--- webapp/routes/configuration/analyse.js
+++ webapp/routes/configuration/analyse.js
@@ -1,6 +1,7 @@
 module.exports = function (app) {
 
-    var controller = app.controllers.configuration;
+    var controller = app.controllers.configuration.analyses;
 
-    app.get('/configuration/analyses', controller.analyse);
+    app.get('/configuration/analyses', controller.index);
+    app.get('/configuration/analyse', controller["new"]);
 }
