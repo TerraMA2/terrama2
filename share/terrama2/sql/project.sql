0bfed29d051c0a0224c8777c991673de2de2a4d9
gqueiroz <gribeiro.mg@gmail.com>
2016-02-23 17:59:47 -0300

    code review: projec.sql

--- share/terrama2/sql/project.sql
+++ share/terrama2/sql/project.sql
@@ -1,44 +1,39 @@
 CREATE SCHEMA terrama2;
 
 CREATE TABLE terrama2.version (
-  "id" serial,
-  "version" integer NOT NULL,
-  "tag" varchar(50) NOT NULL,
-  "description" text,
-  PRIMARY KEY ("id"),
-  UNIQUE(tag)
-);
-
-CREATE TABLE terrama2.project_version (
-  "id" serial,
-  "version" integer NOT NULL,
-  "tag" varchar(50) NOT NULL,
+  "id"          SERIAL,
+  "version"     INTEGER NOT NULL,
+  "tag"         VARCHAR(50) NOT NULL,
+  "description" TEXT NOT NULL,
   PRIMARY KEY ("id"),
   UNIQUE(tag)
 );
 
 CREATE TABLE terrama2.project (
-  "id" serial,
-  "name" varchar(50) NOT NULL,
-  "description" text,
-  "project_version" integer,
+  "id"          SERIAL,
+  "version"     INTEGER NOT NULL,
+  "name"        VARCHAR(50) NOT NULL,
+  "description" TEXT,
   PRIMARY KEY ("id"),
-  FOREIGN KEY(project_version) REFERENCES terrama2.project_version(id),
   UNIQUE(name)
 );
 
-CREATE TABLE terrama2.instance_type (
-  "id" serial,
-  "name" varchar(50) NOT NULL,
+CREATE TABLE terrama2.service_type (
+  "id"   SERIAL,
+  "name" VARCHAR(50) NOT NULL,
   PRIMARY KEY ("id"),
   UNIQUE (name)
 );
 
-CREATE TABLE terrama2.instance (
-  "id" serial,
-  "name"  varchar(50) NOT NULL,
-  "description" text,
-  "instance_type" integer,
+CREATE TABLE terrama2.service_instance (
+  "id"              SERIAL,
+  "name"            VARCHAR(50) NOT NULL,
+  "host"            VARCHAR(50) NOT NULL
+  "port"            INTEGER     NOT NULL,
+  "ssh_user"        VARCHAR(50),
+  "ssh_port"        INTEGER, 
+  "description"     TEXT,
+  "service_type_id" INTEGER,
   PRIMARY KEY ("id"),
-  FOREIGN KEY(instance_type) REFERENCES terrama2.instance_type(id)
+  FOREIGN KEY(service_type_id) REFERENCES terrama2.service_type(id) MATCHES RESTRICT
 );
