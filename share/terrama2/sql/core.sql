154a7574d19ec6f551eaf448594218c06f8b2c52
janosimas <janosimas@gmail.com>
2016-03-07 09:58:13 -0300

    Updating core datamodel script

--- share/terrama2/sql/core.sql
+++ share/terrama2/sql/core.sql
@@ -1,84 +1,134 @@
-CREATE TABLE terrama2.data_provider_scheme (
+
+CREATE SCHEMA terrama2;
+
+CREATE TABLE terrama2.version (
   "id" SERIAL,
-  "name" VARCHAR(50) NOT NULL,
+  "version" INTEGER,
+  "tag" VARCHAR(50),
+  "description" TEXT,
+  PRIMARY KEY ("id"),
+  UNIQUE ("tag")
+);
+
+CREATE TABLE terrama2.project (
+  "id" SERIAL,
+  "version" INTEGER,
+  "name" VARCHAR(50),
   "description" TEXT,
   PRIMARY KEY ("id"),
   UNIQUE(name)
+);
+
+CREATE TABLE terrama2.service_type (
+  "id" SERIAL,
+  "name" TEXT,
+  PRIMARY KEY ("id"),
+  UNIQUE (name)
+);
+
+CREATE TABLE terrama2.service_instance (
+  "id" SERIAL,
+  "service_type_id" INTEGER,
+  "name" VARCHAR(50),
+  "host" VARCHAR(50),
+  "port" INTEGER,
+  "ssh_user" VARCHAR(50),
+  "ssh_port" INTEGER,
+  "description" TEXT,
+  "path_to_binary" TEXT,
+  "number_of_threads" INTEGER,
+  "run_environment" TEXT,
+  PRIMARY KEY ("id"),
+  FOREIGN KEY(service_type_id) REFERENCES terrama2.service_type(id)
 );
 
 CREATE TABLE terrama2.data_provider_type (
   "id" SERIAL,
-  "name" VARCHAR(50) NOT NULL,
+  "name" VARCHAR(50),
   "description" TEXT,
   PRIMARY KEY ("id"),
   UNIQUE(name)
 );
 
-CREATE TABLE terrama2.data_series_type (
-  "id" serial,
-  "name" varchar(50) NOT NULL,
-  "description" text,
-  PRIMARY KEY ("id")
+CREATE TABLE terrama2.data_provider_intent (
+  "id" SERIAL,
+  "name" VARCHAR(50),
+  "description" TEXT,
+  PRIMARY KEY ("id"),
+  UNIQUE(name)
 );
 
 CREATE TABLE terrama2.data_provider (
-  "project" integer,
-  "name" varchar(60) NOT NULL,
-  "description" text,
-  "data_provider_scheme" integer,
-  "data_provider_type" integer,
-  "uri" text NOT NULL,
-  "active" boolean NOT NULL,
-  "id" serial,
+  "project_id" INTEGER,
+  "name" VARCHAR(50),
+  "description" TEXT,
+  "data_provider_type_id" INTEGER,
+  "data_provider_intent_id" INTEGER,
+  "uri" TEXT,
+  "active" BOOLEAN,
+  "id" SERIAL,
   PRIMARY KEY ("id"),
-  FOREIGN KEY(project) REFERENCES terrama2.project(id),
-  FOREIGN KEY(data_provider_scheme) REFERENCES terrama2.data_provider_scheme(id),
-  FOREIGN KEY(data_provider_type) REFERENCES terrama2.data_provider_type(id),
-  UNIQUE(project, name)
+  FOREIGN KEY(project_id) REFERENCES terrama2.project(id),
+  FOREIGN KEY(data_provider_type_id) REFERENCES terrama2.data_provider_type(id),
+  FOREIGN KEY(data_provider_intent_id) REFERENCES terrama2.data_provider_intent(id),
+  UNIQUE(project_id, name)
+);
+
+CREATE TABLE terrama2.data_series_type (
+  "id" SERIAL,
+  "name" VARCHAR(50),
+  "description" TEXT,
+  PRIMARY KEY ("id"),
+  UNIQUE ("name")
+);
+
+CREATE TABLE terrama2.data_formats (
+  "id" SERIAL,
+  "name" VARCHAR(50),
+  "description" TEXT,
+  PRIMARY KEY ("id"),
+  UNIQUE ("name")
+);
+
+CREATE TABLE terrama2.data_series_semantics (
+  "id" SERIAL,
+  "data_format_id" INTEGER,
+  "data_series_type_id" INTEGER,
+  "name" VARCHAR(50),
+  PRIMARY KEY ("id"),
+  UNIQUE ("name"),
+  FOREIGN KEY(data_format_id) REFERENCES terrama2.data_formats(id),
+  FOREIGN KEY(data_series_type_id) REFERENCES terrama2.data_series_type(id)
 );
 
 CREATE TABLE terrama2.data_series (
-  "data_provider" integer,
-  "id" serial,
-  "name" text NOT NULL,
-  "description" text,
-  "data_series_type" integer,
+  "data_provider_id" INTEGER,
+  "data_series_semantics_id" INTEGER,
+  "id" SERIAL,
+  "name" TEXT,
+  "description" TEXT,
   PRIMARY KEY ("id"),
-  FOREIGN KEY(data_provider) REFERENCES terrama2.data_provider(id),
-  FOREIGN KEY(data_series_type) REFERENCES terrama2.data_series_type(id),
+  FOREIGN KEY(data_provider_id) REFERENCES terrama2.data_provider(id),
+  FOREIGN KEY(data_series_semantics_id) REFERENCES terrama2.data_series_semantics(id),
   UNIQUE (name)
 );
 
 CREATE TABLE terrama2.dataset (
-  "id" serial,
-  "data_series" integer,
-  "mask" varchar(255) NOT NULL,
-  "path" varchar(255),
-  "active" boolean NOT NULL,
-  "timezone" text default '+00:00' NOT NULL,
-  "srid" integer,
+  "id" SERIAL,
+  "data_series_id" INTEGER,
+  "active" BOOLEAN,
   PRIMARY KEY ("id"),
-  FOREIGN KEY(data_series) REFERENCES terrama2.data_series(id),
-  FOREIGN KEY(srid) REFERENCES spatial_ref_sys(srid)
+  FOREIGN KEY(data_series_id) REFERENCES terrama2.data_series(id)
 );
 
-CREATE TABLE terrama2.dataset_properties (
-  "id" serial,
-  "dataset" integer,
-  "attribute" text NOT NULL,
-  "alias" text,
-  "position" integer NOT NULL,
+CREATE TABLE terrama2.data_series_properties (
+  "id" INTEGER,
+  "data_series_id" INTEGER,
+  "attribute" TEXT,
+  "alias" TEXT,
+  "position" INTEGER,
   PRIMARY KEY ("id"),
-  FOREIGN KEY(dataset) REFERENCES terrama2.dataset(id),
-  UNIQUE(dataset, attribute),
-  UNIQUE(dataset, alias)
-);
-
-CREATE TABLE terrama2.dataset_metadata (
-  "id" serial,
-  "dataset_id" integer,
-  "key" varchar(50) NOT NULL,
-  "value" text,
-  PRIMARY KEY ("id"),
-  FOREIGN KEY(dataset_id) REFERENCES terrama2.dataset(id)
+  FOREIGN KEY(data_series_id) REFERENCES terrama2.data_series(id),
+  UNIQUE(data_series_id, attribute),
+  UNIQUE(data_series_id, alias)
 );
