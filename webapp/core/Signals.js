e2d0cdd630f74b8efe2035a496f8dc22405ccd76
raphaelrpl <raphael.wcosta@gmail.com>
2016-03-08 09:45:09 -0300

    adding base signals

---
+++ webapp/core/Signals.js
@@ -0,0 +1,12 @@
+/*
+  This file defines TerraMA2 signals for handling TCP communication.
+*/
+
+// todo: define straightforward signals
+var SIGNALS = {
+  Connected: 1,
+  DataProviderAdded: 2,
+  DataSeriesAdded: 3
+};
+
+module.exports = SIGNALS;
