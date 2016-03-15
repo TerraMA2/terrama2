dd574539491ed8f0984f568bc5da995993283549
janosimas <janosimas@gmail.com>
2016-03-15 16:05:05 -0300

    DataSet for occurrences

--- src/terrama2/core/data-model/DataSetOccurrence.hpp
+++ src/terrama2/core/data-model/DataSetOccurrence.hpp
@@ -20,29 +20,27 @@
 */
 
 /*!
-  \file terrama2/core/data-model/DataSetDcp.hpp
+  \file terrama2/core/data-model/DataSetOccurrence.hpp
 
   \author Jano Simas
 */
 
-#ifndef __TERRAMA2_CORE_DATA_MODEL_DATA_SET_DCP_HPP__
-#define __TERRAMA2_CORE_DATA_MODEL_DATA_SET_DCP_HPP__
+#ifndef __TERRAMA2_CORE_DATA_MODEL_DATA_SET_OCCURRENCE_HPP__
+#define __TERRAMA2_CORE_DATA_MODEL_DATA_SET_OCCURRENCE_HPP__
 
 #include "DataSet.hpp"
-
-//Terralib
-#include <terralib/geometry/Point.h> 
 
 namespace terrama2
 {
   namespace core
   {
-    struct DataSetDcp : public DataSet
+    struct DataSetOccurrence : public DataSet
     {
-      te::gm::Point position;//TODO: A posição é sempre um ponto? pode ser uma região?
       std::string dateTimeColumnName;
+      std::string geometryColumnName;
+      uint64_t srid;
     };
   } // end namespace core
 } // end namespace terrama2
 
-#endif // __TERRAMA2_CORE_DATA_MODEL_DATA_SERIES_DCP_HPP__
+#endif // __TERRAMA2_CORE_DATA_MODEL_DATA_SET_OCCURRENCE_HPP__
