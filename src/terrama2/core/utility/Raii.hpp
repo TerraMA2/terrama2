f23b9f5ccd128c200c522ba40b3d1e0f37597725
edelatin <evandro.delatin@gmail.com>
2016-03-15 16:42:51 -0300

    Adding CurlOpener in Class Raii

--- src/terrama2/core/utility/Raii.hpp
+++ src/terrama2/core/utility/Raii.hpp
@@ -28,6 +28,10 @@
 //STd
 #include <exception>
 
+// Libcurl
+#include <curl/curl.h>
+
+
 namespace terrama2
 {
   namespace collector
@@ -131,6 +135,46 @@ namespace terrama2
     private:
       std::FILE* file_;
     };
+
+    //! Class for Resource Acquisition Is Initialization (RAII) of Curl.
+    class CurlOpener
+    {
+      public:
+
+       //! Constructor.
+       CurlOpener()
+       {
+         curl_ = curl_easy_init();
+       } 
+
+       /*!
+           The init function performs the function curl_easy_cleanup closing all handle connections 
+           curl and then performs the initialization of the curl.
+       */
+       void init()
+       {
+         curl_easy_cleanup(curl_);
+         curl_ = curl_easy_init();
+       }
+
+       //! Assume ownership of curl.
+       CURL* fcurl() const
+       {
+         return curl_;
+       }
+
+       /*! When CurlOpener destructor is called, the function curl_easy_cleanup is used automatically. 
+           The function curl_easy_cleanup close all connections this handle curl.
+       */
+       ~CurlOpener()
+       {
+         curl_easy_cleanup(curl_);
+       }
+
+      private:
+        CURL* curl_; //!< Attribute for Handler Curl.
+     };
+
   }
 }
 
