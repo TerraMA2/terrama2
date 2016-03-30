1bfb323fa850d0874dda2582ae5630c8e5df4197
pauloremoli <paulo002@gmail.com>
2016-03-30 14:20:45 -0300

    Schedule model

--- src/terrama2/core/typedef.hpp
+++ src/terrama2/core/typedef.hpp
@@ -32,7 +32,7 @@
 
 typedef uint64_t ServiceInstanceId;
 typedef uint64_t ProjectId;
-
+typedef uint64_t ScheduleId;
 typedef uint64_t DataSetId;
 typedef uint64_t DataSeriesId;
 typedef uint64_t DataProviderId;
