376ed1abca0bc44fdc393a6b3f20c6e6e2f5fd14
raphaelrpl <raphael.wcosta@gmail.com>
2016-03-31 17:49:36 -0300

    adding i18n module for angular

--- webapp/app.js
+++ webapp/app.js
@@ -7,8 +7,19 @@ var express = require('express'),
     app = express(),
     load = require('express-load'),
     swig = require('swig'),
-    i18n = require('i18n-2'),
+    // i18n = require('i18n-2'),
+    i18n = require( "i18n" );
+    i18nRoutes = require( "i18n-node-angular" );
     server = require('http').Server(app);
+
+
+i18n.configure( {
+  // setup some locales - other locales default to en silently
+  locales        : [ "en", "pt", "fr", "es" ],
+  directory      : __dirname + "/locales",
+  objectNotation : true
+} );
+
 
 // Set SWIG template engine
 var customSwig = new swig.Swig({varControls: ["{[", "]}"]});
@@ -18,22 +29,29 @@ app.set('view engine', 'html');
 // Set template directories
 app.set('views', __dirname + '/views');
 
+// The order of these calls matters!
+app.use(i18n.init);
+app.use(i18nRoutes.getLocale);
+
+// This line should be removed with express 4.0
+i18nRoutes.configure(app, {"extension": ".js", directory : __dirname + "/locales/"});
+
 // Set up the internacionalization
-i18n.expressBind(app, {
-  // setup some locales - other locales default to vi silently
-  locales: ['pt', 'en', 'es'],
-  // set the default locale
-  defaultLocale: 'en',
-  // set the cookie name
-  cookieName: 'locale',
-  // allow to change language by query string. i.e: url?lang=es
-  query: true
-});
+// i18n.expressBind(app, {
+//   // setup some locales - other locales default to vi silently
+//   locales: ['pt', 'en', 'es'],
+//   // set the default locale
+//   defaultLocale: 'en',
+//   // set the cookie name
+//   cookieName: 'locale',
+//   // allow to change language by query string. i.e: url?lang=es
+//   query: true
+// });
 
 // set up the internacionalization middleware
 app.use(function(req, res, next) {
-  req.i18n.setLocaleFromQuery();
-  req.i18n.setLocaleFromCookie();
+  // req.i18n.setLocaleFromQuery();
+  // req.i18n.setLocaleFromCookie();
   next();
 });
 
