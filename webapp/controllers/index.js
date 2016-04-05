80521238922c8772475f55b3905a774618a04fe1
raphaelrpl <raphael.wcosta@gmail.com>
2016-04-05 10:18:52 -0300

    template review: redirects, icons and images

--- webapp/controllers/index.js
+++ webapp/controllers/index.js
@@ -1,6 +1,8 @@
 module.exports = function(app) {
 
   function indexController(request, response) {
+    app.locals.activeProject = {};
+    app.locals.user = {};
     return response.redirect("/login");
   }
 
--- webapp/routes/configuration/Alerts.js
+++ webapp/routes/configuration/Alerts.js
@@ -3,5 +3,5 @@ module.exports = function (app) {
     var controller = app.controllers.configuration.Alerts;
 
     app.get('/configuration/alerts', controller.index);
-    app.get('/configuration/alert', controller["new"]);
+    app.get('/configuration/alerts/new', controller["new"]);
 }
