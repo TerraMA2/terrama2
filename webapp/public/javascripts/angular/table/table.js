64e266ec1cb5b99dc7d5df3e5ca5faabf38337c0
raphaelrpl <raphael.wcosta@gmail.com>
2016-04-04 23:47:21 -0300

    adding map-pin icon, and template icons in angular table

--- webapp/public/javascripts/angular/table/table.js
+++ webapp/public/javascripts/angular/table/table.js
@@ -8,13 +8,20 @@ angular.module('terrama2.table', ['terrama2'])
         model: '=model',
         link: '&',
         icon: '&',
+        iconProperties: '=iconProperties',
         linkToAdd: '=linkToAdd'
       },
       
       controller: function($scope, i18n) {
         $scope.i18n = i18n;
         $scope.searchInput = '';
-        
+
+        if (!$scope.iconProperties)
+          $scope.iconProperties = {type: 'img'};
+
+        $scope.width = $scope.iconProperties.width || 24;
+        $scope.width = $scope.iconProperties.height || 24;
+
         $scope.isFunction = function() {
           return angular.isFunction(icon());
         };
