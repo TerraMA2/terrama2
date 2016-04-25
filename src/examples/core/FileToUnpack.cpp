9be6eae2a7c696ba7c55214ae9ed9f01fa912d49
edelatin <evandro.delatin@gmail.com>
2016-04-25 15:15:51 -0300

    Adding Unpack Class and Unpack example.

---
+++ src/examples/core/FileToUnpack.cpp
@@ -0,0 +1,61 @@
+
+// TerraMA2
+#include <terrama2/core/utility/Utils.hpp>
+#include <terrama2/core/utility/Unpack.hpp>
+
+// STL
+#include <iostream>
+
+// QT
+#include <QUrl>
+#include <QObject>
+#include <QDir>
+#include <QFile>
+#include <QDebug>
+#include <QStringList>
+
+
+int main(int argc, char* argv[])
+{  
+  std::string uri;
+  std::string path;
+  uri = "file://";
+  uri += TERRAMA2_DATA_DIR;
+  uri += "/PCD_serrmar_INPE/";
+  uri += "SetoresCubatao_UTM_sad69.zip";
+  //uri += "30885.txt";
+
+  terrama2::core::Unpack unpack;
+
+  if(!unpack.verifyCompressFile(uri))
+  {
+    qDebug() << "File not compressed!";
+  }
+  else
+  {
+    path = unpack.unpackList(uri);
+    qDebug() << "File descompressed!";
+
+    QUrl uriLocal(path.c_str());
+    QDir dir(uriLocal.path());
+
+    dir.setNameFilters(QStringList()<<"*.*");
+    QStringList fileList = dir.entryList();
+
+    if (fileList.empty())
+      qDebug() << "Test failed!";
+    else
+    {
+      for (QString nameFile: fileList)
+      {
+        qDebug() << "Extracting: " << nameFile;
+      }
+
+      // Remove paste of download files.
+      if (dir.exists())
+        dir.removeRecursively();
+    }
+  }
+
+  return 0;
+}  
