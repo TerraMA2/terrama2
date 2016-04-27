121a0815029f120123373daf8cec289f2f069fc7
raphaelrpl <raphael.wcosta@gmail.com>
2016-04-27 17:12:22 -0300

    adding session for collapse bar

--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -2,4 +2,9 @@ module.exports = function (app) {
   var controller = app.controllers.index;
 
   app.get('/', controller);
-}
+
+  app.post('/userDefaults', function(request, response) {
+    app.locals.collapsed = request.body.collapsed;
+    return response.json({status: 200});
+  });
+};
