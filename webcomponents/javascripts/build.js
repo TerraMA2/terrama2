a648ce71873e6e28b0df2f23882a1b0c3c9f10bd
Jean Souza <jeancfsouza@gmail.com>
2016-04-29 17:38:41 -0300

    build update

---
+++ webcomponents/javascripts/build.js
@@ -0,0 +1,12 @@
+({
+  baseUrl: ".",
+  out: "TerraMA2WebComponents.min.js",
+  paths: {
+    TerraMA2WC: "."
+  },
+  include: [
+    'TerraMA2WC/TerraMA2WebComponents',
+    'TerraMA2WC/components/LayerExplorer.TerraMA2WebComponents',
+    'TerraMA2WC/components/MapDisplay.TerraMA2WebComponents'
+  ]
+})
