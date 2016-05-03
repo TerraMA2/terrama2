449baabacea48a3e5faa9c9f0aa9d0c9424bbb08
Jean Souza <jeancfsouza@gmail.com>
2016-05-03 17:49:23 -0300

    Creation of the Python script checker

--- webapp/public/javascripts/terrama2.js
+++ webapp/public/javascripts/terrama2.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
+  var socket = io(window.location.origin);
 
+  socket.on('checkPythonScriptResponse', function(result) {
+    console.log(result);
+  });
+
+  $('#checkAnalysisScript').on('click', function() {
+    socket.emit('checkPythonScriptRequest', { script: $('textarea[name="script"]').val() });
+  });
 });
