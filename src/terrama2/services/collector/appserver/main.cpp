9a5969c203b53564133a9bbf8445941c665286ba
janosimas <janosimas@gmail.com>
2016-05-06 17:15:47 -0300

    log connection info in services

--- src/terrama2/services/collector/appserver/main.cpp
+++ src/terrama2/services/collector/appserver/main.cpp
@@ -31,7 +31,6 @@
 #include <terrama2/services/collector/core/Service.hpp>
 #include <terrama2/services/collector/core/DataManager.hpp>
 #include <terrama2/core/network/TcpManager.hpp>
-#include <terrama2/core/data-access/DataRetriever.hpp>
 #include <terrama2/core/utility/Utils.hpp>
 #include <terrama2/core/utility/ServiceManager.hpp>
 #include <terrama2/impl/Utils.hpp>
@@ -74,6 +73,15 @@ int main(int argc, char* argv[])
     auto& serviceManager = terrama2::core::ServiceManager::getInstance();
     serviceManager.setServiceType("Collector");
     serviceManager.setListeningPort(listeningPort);
+    std::map<std::string, std::string> connInfo { {"PG_HOST", "localhost"},
+                                                  {"PG_PORT", "5432"},
+                                                  {"PG_USER", "postgres"},
+                                                  {"PG_PASSWORD", "postgres"},
+                                                  {"PG_DB_NAME", "nodejs"},
+                                                  {"PG_CONNECT_TIMEOUT", "4"},
+                                                  {"PG_CLIENT_ENCODING", "UTF-8"}
+                                                };
+    serviceManager.setLogConnectionInfo(connInfo);
 
     terrama2::core::TcpManager tcpManager(dataManager);
     tcpManager.listen(QHostAddress::Any, serviceManager.listeningPort());
