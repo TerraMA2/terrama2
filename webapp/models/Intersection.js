eba369ad1879a23045a5b585c51f9eb1f1c1234e
raphaelrpl <raphael.wcosta@gmail.com>
2016-06-15 13:32:19 -0300

    adding intersection tab in dataseries dynamic registration

---
+++ webapp/models/Intersection.js
@@ -0,0 +1,43 @@
+module.exports = function(sequelize, DataTypes) {
+  var Intersection = sequelize.define("Intersection",
+    {
+      id: {
+       type: DataTypes.INTEGER,
+       allowNull: false,
+       primaryKey: true,
+       autoIncrement: true
+      },
+      attribute: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    },
+    {
+      underscored: true,
+      underscoredAll: true,
+      timestamps: false,
+
+      classMethods: {
+        associate: function(models) {
+          Intersection.belongsTo(models.Collector, {
+            onDelete: "CASCADE",
+            foreignKey: {
+              name: 'collector_id',
+              allowNull: false
+            }
+          });
+
+          Intersection.belongsTo(models.DataSeries, {
+            onDelete: "CASCADE",
+            foreignKey: {
+              name: 'dataseries_id',
+              allowNull: false
+            }
+          });
+        }
+      }
+    }
+  );
+
+  return Intersection;
+};
--- webapp/public/javascripts/angular/app.js
+++ webapp/public/javascripts/angular/app.js
@@ -7,3 +7,35 @@ terrama2Application.factory("TryCaster", function() {
     return parseInt(value);
   }
 });
+
+// Helper for display invalid fields from form
+terrama2Application.factory('FormHelper', function() {
+  return function(form) {
+    angular.forEach(form.$error, function (field) {
+      angular.forEach(field, function(errorField){
+        errorField.$setDirty();
+      })
+    });
+  }
+})
+
+terrama2Application.factory('$HttpSync', ['$http', '$cacheFactory',
+  function ($http, $cacheFactory) {
+    var cache = $cacheFactory('$HttpSync');
+
+    var _wrap = function(promise) {
+      return promise.then(function(response) {
+        return response.data;
+      })
+    }
+
+    return {
+      get: function $httpOnce(url, options) {
+        return _wrap(cache.get(url) || cache.put(url, $http.get(url, options)));
+      },
+      post: function $httpOnce(url, options) {
+        return _wrap(cache.get(url) || cache.put(url, $http.post(url, options)));
+      }
+    }
+  }
+]);
