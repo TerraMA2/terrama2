6fd68eb6e03ff46527c8cf0c2e6fb4917791b58e
raphaelrpl <raphael.wcosta@gmail.com>
2016-09-05 14:09:49 -0300

    enabling specify contexts for terrama2 web application. To use it, call npm start mydatabase (be sure that 'mydatabase' is in config/config.terrama2

--- webapp/config/Database.js
+++ webapp/config/Database.js
@@ -4,41 +4,88 @@ var util = require('util');
 var fs = require('fs');
 var path = require('path');
 var Sequelize = require('./Sequelize');
+var Connection = require("sequelize").Connection;
 
-
+/**
+ * @type {Connection}
+ */
 var sequelize = null;
 
 /**
  * It is a TerraMA2 Database initialization module.
+ * 
+ * @class Database
  */
 var Database = function(pathToConfig) {
-  if (pathToConfig === undefined || !pathToConfig)
+  if (pathToConfig === undefined || !pathToConfig) {
     pathToConfig = path.join(__dirname, 'config.terrama2');
+  }
 
-  // reading terrama2 database file configuration
+  /**
+   * It stores a config.terrama2 settings
+   * @type {Object} config
+   */
   this.config = JSON.parse(fs.readFileSync(pathToConfig, 'utf-8'));
 };
 
+/**
+ * It retrieves if orm is already initialized
+ * 
+ * @returns {boolean}
+ */
 Database.prototype.isInitialized = function() {
   return sequelize !== null;
 };
 
+/**
+ * It retrieves a orm instance
+ * 
+ * @returns {Connection}
+ */
 Database.prototype.getORM = function() {
   return sequelize;
 };
 
-Database.prototype.init = function() {
+/**
+ * It initializes database, creating database, schema and postgis support. Once prepared, it retrieves a ORM instance
+ * 
+ * @param {string?} context - It represents which config context should be initialized. {@link webapp/config/config.terrama2}
+ * @returns {Promise<Connection>}
+ */
+Database.prototype.init = function(context) {
   var self = this;
+
+  // If there is no context, set to "default"
+  if (!context) {
+    console.info("No context selected. It will use \"default\". To change it, call npm start mycustomcontext. Be sure that your custom context exists in config/config.terrama2")
+    context = "default";
+  }
+
   return new Promise(function(resolve, reject) {
-    if (self.isInitialized())
+    if (self.isInitialized()) {
       return resolve(sequelize);
+    }
 
-    var schema = self.config.define.schema;
-    var databaseName = self.config.database;
-    var baseUri = util.format("postgres://%s:%s@%s", self.config.username, self.config.password, self.config.host);
+    // checking if there is a context in configuration file
+    if (self.config && !self.config.hasOwnProperty(context)) {
+      var msg = util.format("\"%s\" not found in configuration file. Please check \"webapp/config/config.terrama2\"", context);
+      return reject(new Error(msg));
+    }
 
-    if (self.config.port)
-      baseUri += ":" + self.config.port;
+    /**
+     * Current database configuration context
+     * 
+     * @type {Object}
+     */
+    var contextConfig = self.config[context];
+
+    var schema = contextConfig.define.schema;
+    var databaseName = contextConfig.database;
+    var baseUri = util.format("postgres://%s:%s@%s", contextConfig.username, contextConfig.password, contextConfig.host);
+
+    if (contextConfig.port) {
+      baseUri += ":" + contextConfig.port;
+    }
 
     var uri = baseUri;
 
@@ -64,7 +111,7 @@ Database.prototype.init = function() {
             }
 
             cli.query("CREATE EXTENSION postgis", function(err) {
-              sequelize = new Sequelize(self.config);
+              sequelize = new Sequelize(contextConfig);
               cli.end();
               resolve(sequelize);
             });
@@ -74,18 +121,24 @@ Database.prototype.init = function() {
 
       client.query(databaseQuery, function(err) {
         if (err) {
-          if (err.code === "42P04")
+          if (err.code === "42P04") {
             console.log(err.toString());
-          else
+          } else {
             console.log(err);
+          }
         }
         client.end();
         _createSchema();
-      })
+      });
     });
   });
 };
 
+/**
+ * It closes database orm connection
+ * 
+ * @returns {Promise}
+ */
 Database.prototype.finalize = function() {
   var self = this;
   return new Promise(function(resolve, reject) {
