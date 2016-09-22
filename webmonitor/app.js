53350a76d5dbfa5d5e38195065dc81910aa31ad5
franciscovn <franciscovn909@gmail.com>
2016-09-22 17:00:16 -0300

    Using bower to manage web packages

--- webmonitor/app.js
+++ webmonitor/app.js
@@ -24,6 +24,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
