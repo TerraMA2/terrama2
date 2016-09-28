f4d0fecc6a495eeb51b235c9ea6fd2678a68bcf7
raphaelrpl <raphael.wcosta@gmail.com>
2016-09-28 10:59:30 -0300

    adding views pages

--- webapp/routes/configuration/Views.js
+++ webapp/routes/configuration/Views.js
@@ -1,7 +1,9 @@
-var passport = require('../../config/Passport');
-
 module.exports = function (app) {
-  var controller = app.controllers.configuration.MapDisplay;
+  'use strict';
+
+  var passport = require('../../config/Passport');
+  var controller = app.controllers.configuration.Views;
 
   app.get('/configuration/views', passport.isAuthenticated, controller.get);
+  app.get('/configuration/views/new', passport.isAuthenticated, controller.new);
 };
