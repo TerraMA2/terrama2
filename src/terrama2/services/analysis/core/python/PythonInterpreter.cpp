1b23b23a96a59df7ab6a9283bdfae2bf69247493
pauloremoli <paulo0002@gmail.com>
2016-09-28 15:31:34 -0300

    Fix memory leak in monitored object analysis

--- src/terrama2/services/analysis/core/python/PythonInterpreter.cpp
+++ src/terrama2/services/analysis/core/python/PythonInterpreter.cpp
@@ -152,8 +152,11 @@ void terrama2::services::analysis::core::python::runMonitoredObjectScript(PyThre
         throw PythonInterpreterException() << terrama2::ErrorDescription(errMsg.c_str());
       }
 
+      Py_DECREF(pValueIndex);
+
     }
 
+    Py_DECREF(pValueAnalysis);
     Py_DECREF(pModule);
     Py_DECREF(pCompiledFn);
   }
