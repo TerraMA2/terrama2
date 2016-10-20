4cd6fd8cc3c75ff87baacc616ec0865e60819b9b
raphaelrpl <raphael.wcosta@gmail.com>
2016-10-20 13:40:36 -0200

    fixing multiple calls on submit button

--- webapp/core/DataManager.js
+++ webapp/core/DataManager.js
@@ -1445,7 +1445,66 @@ var DataManager = module.exports = {
       });
     });
   },
+  /**
+   * It retrieves all data set formats from database
+   * 
+   * @param {Object} restriction - A query restriction
+   * @param {Object} options - A query options
+   * @param {Transaction} options.transaction - An ORM transaction
+   * @returns {Promise} - a 'bluebird' module with DataSeries instance or error callback
+   */
+  listDataSetFormats: function(restriction, options) {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+      return models.db.DataSetFormat
+        .findAll(Utils.extend({
+          where: restriction
+        }, options))
+        .then(function(dataSetFormats) {
+          return resolve(dataSetFormats);
+        })
 
+        .catch(function(err) {
+          return reject(new Error(Utils.format("Could not retrieve data set formats %s", err.toString())));
+        });
+    });
+  },
+  /**
+   * It performs Insert or Update operation in database. If data set format found with given restriction, it applies
+   * update operation. Otherwise, it performs insert operation
+   * 
+   * @param {Object} restriction - A query restriction
+   * @param {Object} dataSetFormat - A data set format values
+   * @param {Object} options - A query options
+   * @param {Transaction} options.transaction - An ORM transaction
+   * @return {Promise} - a 'bluebird' module with DataSeries instance or error callback
+   */
+  upsertDataSetFormats: function(restriction, dataSetFormat, options) {
+    var self = this;
+    return new Promise(function(resolve, reject) {
+      return self
+        .listDataSetFormats(restriction, Utils.extend({limit: 1}, options))
+        .then(function(dataSetFormats) {
+          if (dataSetFormats.length === 0) {
+            // insert
+            return models.db.DataSetFormat.create(dataSetFormat, options); 
+          } else {
+            return models.db.DataSetFormat.update(dataSetFormat, Utils.extend({
+              fields: ["key", "value", "data_set_id"],
+              where: {id: dataSetFormats[0].id}
+            }, options));
+          }
+        })
+
+        .then(function() {
+          return resolve();
+        })
+
+        .catch(function(err) {
+          return reject(new Error(Utils.format("Could not upsert data set format %s", err.toString())));
+        });
+    })
+  },
   /**
    * It updates a DataSeries object. It should be an object containing object filled out with identifier
    * and model values.
@@ -1461,29 +1520,69 @@ var DataManager = module.exports = {
     return new Promise(function(resolve, reject) {
       var dataSeries = Utils.find(self.data.dataSeries, {id: dataSeriesId});
 
-      if (dataSeries) {
-        models.db.DataSeries.update(dataSeriesObject, Utils.extend({
-          fields: ['name', 'description', 'data_provider_id'],
-          where: {
-            id: dataSeriesId
-          }
-        }, options)).then(function() {
-          return self.getDataProvider({id: parseInt(dataSeriesObject.data_provider_id)}).then(function(dataProvider) {
-            dataSeries.name = dataSeriesObject.name;
-            dataSeries.description = dataSeriesObject.description;
-            dataSeries.data_provider_id = dataProvider.id;
-
-            return resolve(new DataModel.DataSeries(dataSeries));
-          }).catch(function(err) {
-            console.log(err);
-            return reject(err);
-          });
-        }).catch(function(err) {
-          return reject(new exceptions.DataSeriesError("Could not update data series ", err.errors));
-        });
-      } else {
+      if (!dataSeries) {
         return reject(new exceptions.DataSeriesError("Data series not found. ", []));
       }
+
+      return models.db.DataSeries.update(dataSeriesObject, Utils.extend({
+        fields: ['name', 'description', 'data_provider_id'],
+        where: {
+          id: dataSeriesId
+        }
+      }, options))
+      // on data series update, update format
+        .then(function() {
+          return self.getDataSeriesSemantics({id: dataSeriesObject.data_series_semantic_id}, options);
+        })
+
+        .then(function(dataSeriesSemantics) {
+          if (dataSeriesSemantics.data_series_type_name !== Enums.DataSeriesType.DCP) {              
+            var oldDataSet = dataSeries.dataSets[0];
+            var newDataSet = dataSeriesObject.dataSets[0];
+            /**
+             * It defines a list of promises to perform DB operation. It may be insert or update
+             * @type {Promise[]}
+             */
+            var promisesFormat = Utils.generateArrayFromObject(newDataSet.format, function(key, value) {
+              var obj = {
+                key: key,
+                value: value,
+                data_set_id: oldDataSet.id
+              };
+              // performs Insert or Update
+              return self.upsertDataSetFormats({
+                data_set_id: oldDataSet.id,
+                key: key
+              }, obj, options);
+            });
+
+            return Promise.all(promisesFormat)
+              .then(function() {
+                return self
+                  .listDataSetFormats({data_set_id: oldDataSet.id}, options)
+                  .then(function(newDataSetFormats) {
+                    oldDataSet.format = Utils.formatMetadataFromDB(newDataSetFormats);
+                    return null;
+                  });
+              });
+          } else {
+            return null;
+          }
+        })
+
+        .then(function() {
+          return self.getDataProvider({id: parseInt(dataSeriesObject.data_provider_id)});
+        })
+        // retrieving data provider
+        .then(function(dataProvider) {
+          dataSeries.name = dataSeriesObject.name;
+          dataSeries.description = dataSeriesObject.description;
+          dataSeries.data_provider_id = dataProvider.id;
+
+          return resolve(new DataModel.DataSeries(dataSeries));
+        }).catch(function(err) {
+          return reject(new exceptions.DataSeriesError(Utils.format("Could not update data series %s", err.toString()), err.errors));
+        });
     });
   },
 
--- webapp/public/javascripts/angular/data-series/registration.js
+++ webapp/public/javascripts/angular/data-series/registration.js
@@ -29,11 +29,11 @@ angular.module('terrama2.dataseries.registration', [
       .state('wizard', {
         parent: 'main',
         url: "/wizard",
-        templateUrl: '/javascripts/angular/wizard.html'
+        templateUrl: '/javascripts/angular/wizard.html',
       }).state('advanced', {
         parent: 'main',
         url: "/advanced",
-        templateUrl: '/javascripts/angular/advanced.html'
+        templateUrl: '/javascripts/angular/advanced.html',
       }
     );
   }])
@@ -1025,6 +1025,215 @@ angular.module('terrama2.dataseries.registration', [
         $scope.display = false;
       };
 
+      // it dispatches post operation to nodejs
+      var _sendRequest = function(object) {
+        var request = null;
+        var data = {
+          dataSeries: object.dataToSend,
+          schedule: object.scheduleValues,
+          filter: object.filterValues,
+          service: object.serviceOutput,
+          intersection: object.intersection,
+          active: object.active
+        };
+        if ($scope.isUpdating) {
+          request = DataSeriesFactory.put(configuration.dataSeries.input.id, data);
+        } else {
+          request = DataSeriesFactory.post(data);
+        }
+
+        request.then(function(data) {
+          console.log(data);
+          $window.location.href = "/configuration/" + configuration.dataSeriesType + "/dataseries?token=" + (data.token || data.data.token);
+        }).catch(function(err) {
+          $scope.alertLevel = "alert-danger";
+          $scope.alertBox.message = err.message || err.data.message;
+          $scope.display = true;
+          $scope.extraProperties = {};
+          console.log(err);
+        });
+      };
+
+      $scope.$on("storageValuesReceive", function(event, values) {
+        //  todo: improve
+        var dSemantics = Object.assign({}, $scope.dataSeries.semantics);
+        var dataObject = _save();
+        var dSeriesName = dataObject.dataSeries.name;
+        // setting _input in data series
+        dataObject.dataSeries.name += "_input";
+
+        var dSets = values.data;
+
+        // it makes data set format
+        var _makeFormat = function(dSetObject) {
+          var format_ = {};
+          for(var key in dSetObject) {
+            if (dSetObject.hasOwnProperty(key) && key.toLowerCase() !== "id")
+              format_[key] = dSetObject[key];
+          }
+
+          // adding extra metadata
+          if (dSemantics.metadata.metadata && Object.keys(dSemantics.metadata.metadata).length > 0)
+            Object.assign(format_, dSemantics.metadata.metadata);
+
+          return format_;
+        };
+
+        var out;
+        if (dSets instanceof Array) {
+          // setting to active
+          var dSetsLocal = [];
+          dSets.forEach(function(dSet) {
+            dSetsLocal.push({
+              active: $scope.dataSeries.active,
+              format: _makeFormat(dSet)
+            });
+          });
+          out = dSetsLocal;
+        } else {
+          dSets.format = _makeFormat(dSets);
+          dSets.active = $scope.dataSeries.active;
+          out = [dSets];
+        }
+
+        // preparing intersection
+        var intersectionValues = [];
+        for(var k in $scope.intersection) {
+          if ($scope.intersection.hasOwnProperty(k)) {
+            if (!$scope.intersection[k].selected) {
+              continue;
+            }
+            var attributes = $scope.intersection[k].attributes;
+            var dataseries_id = $scope.intersection[k].data_series.id;
+
+            // grid
+            if (attributes.length === 0) {
+              intersectionValues.push({
+                dataseries_id: dataseries_id
+              });
+            }
+
+            for(var i = 0; i < attributes.length; ++i) {
+              var attribute = attributes[i];
+              intersectionValues.push({
+                attribute: attribute,
+                dataseries_id: dataseries_id
+              });
+            }
+          }
+        }
+
+        var outputDataSeries = {
+          name: dSeriesName,
+          description: dataObject.dataSeries.description,
+          data_series_semantic_id: values.semantics.id,
+          data_provider_id: values.data_provider,
+          dataSets: out
+        };
+
+        _sendRequest({
+          dataToSend: {input: dataObject.dataSeries, output: outputDataSeries},
+          scheduleValues: dataObject.schedule,
+          filterValues: dataObject.filter,
+          serviceOutput: values.service,
+          intersection: intersectionValues,
+          active: dataObject.dataSeries.active
+        });
+      });
+
+      // it prepares dataseries object, schedule and filter object
+      var _save = function() {
+
+        var dataToSend = Object.assign({}, $scope.dataSeries);
+        dataToSend.data_series_semantic_id = $scope.dataSeries.semantics.id;
+
+        var semantics = Object.assign({}, dataToSend.semantics);
+        delete dataToSend.semantics;
+
+        dataToSend.dataSets = [];
+
+        $scope.errorFound = false;
+
+        switch(semantics.data_series_type_name) {
+          case "DCP":
+            $scope.dcps.forEach(function(dcp) {
+              var format = {};
+              for(var key in dcp) {
+                if (dcp.hasOwnProperty(key))
+                  if (key !== "latitude" && key !== "longitude" && key !== "active")
+                    format[key] = dcp[key];
+              }
+              var dataSetStructure = {
+                active: $scope.dataSeries.active,
+                format: format,
+                position: {
+                  type: 'Point',
+                  coordinates: [dcp.latitude, dcp.longitude],
+                  crs: {
+                    type: 'name',
+                    properties : {
+                      name: "EPSG:" + dcp.projection
+                    }
+                  }
+                }
+              };
+
+              dataToSend.dataSets.push(dataSetStructure);
+            });
+
+            break;
+          case "OCCURRENCE":
+          case "GRID":
+          case "STATIC_DATA":
+            var format = $scope.model;
+
+            var dataSet = {
+              semantics: semantics,
+              active: $scope.dataSeries.active,
+              format: format
+            };
+            dataToSend.dataSets.push(dataSet);
+            break;
+
+          default:
+            break;
+        }
+
+        var filterValues = Object.assign({}, $scope.filter);
+        if ($scope.filter.filterArea === $scope.filterTypes.AREA.value) {
+          filterValues.region = Polygon.build($scope.filter.area || {});
+        }
+
+        var scheduleValues = Object.assign({}, $scope.schedule);
+        switch(scheduleValues.scheduleHandler) {
+          case "seconds":
+          case "minutes":
+          case "hours":
+            scheduleValues.frequency_unit = scheduleValues.scheduleHandler;
+            scheduleValues.frequency_start_time = scheduleValues.frequency_start_time ? moment(scheduleValues.frequency_start_time).format("HH:mm:ssZ") : "";
+            break;
+          case "weeks":
+          case "monthly":
+          case "yearly":
+            // todo: verify
+            var dt = scheduleValues.schedule_time;
+            scheduleValues.schedule_unit = scheduleValues.scheduleHandler;
+            scheduleValues.schedule_time = moment(dt).format("HH:mm:ss");
+            break;
+
+          default:
+            break;
+        }
+
+        console.log(dataToSend);
+
+        return {
+          dataSeries: dataToSend,
+          schedule: scheduleValues,
+          filter: filterValues
+        };
+      };
+
       $scope.save = function() {
         $scope.extraProperties = {};
         $scope.$broadcast('formFieldValidation');
@@ -1089,216 +1298,7 @@ angular.module('terrama2.dataseries.registration', [
 
         $scope.alertBox.title = "Data Series Registration";
 
-        // it prepares dataseries object, schedule and filter object
-        var _save = function() {
-
-          var dataToSend = Object.assign({}, $scope.dataSeries);
-          dataToSend.data_series_semantic_id = $scope.dataSeries.semantics.id;
-
-          var semantics = Object.assign({}, dataToSend.semantics);
-          delete dataToSend.semantics;
-
-          dataToSend.dataSets = [];
-
-          $scope.errorFound = false;
-
-          switch(semantics.data_series_type_name) {
-            case "DCP":
-              $scope.dcps.forEach(function(dcp) {
-                var format = {};
-                for(var key in dcp) {
-                  if (dcp.hasOwnProperty(key))
-                    if (key !== "latitude" && key !== "longitude" && key !== "active")
-                      format[key] = dcp[key];
-                }
-                var dataSetStructure = {
-                  active: $scope.dataSeries.active,
-                  format: format,
-                  position: {
-                    type: 'Point',
-                    coordinates: [dcp.latitude, dcp.longitude],
-                    crs: {
-                      type: 'name',
-                      properties : {
-                        name: "EPSG:" + dcp.projection
-                      }
-                    }
-                  }
-                };
-
-                dataToSend.dataSets.push(dataSetStructure);
-              });
-
-              break;
-            case "OCCURRENCE":
-            case "GRID":
-            case "STATIC_DATA":
-              var format = $scope.model;
-
-              var dataSet = {
-                semantics: semantics,
-                active: $scope.dataSeries.active,
-                format: format
-              };
-              dataToSend.dataSets.push(dataSet);
-              break;
-
-            default:
-              break;
-          }
-
-          var filterValues = Object.assign({}, $scope.filter);
-          if ($scope.filter.filterArea === $scope.filterTypes.AREA.value) {
-            filterValues.region = Polygon.build($scope.filter.area || {});
-          }
-
-          var scheduleValues = Object.assign({}, $scope.schedule);
-          switch(scheduleValues.scheduleHandler) {
-            case "seconds":
-            case "minutes":
-            case "hours":
-              scheduleValues.frequency_unit = scheduleValues.scheduleHandler;
-              scheduleValues.frequency_start_time = scheduleValues.frequency_start_time ? moment(scheduleValues.frequency_start_time).format("HH:mm:ssZ") : "";
-              break;
-            case "weeks":
-            case "monthly":
-            case "yearly":
-              // todo: verify
-              var dt = scheduleValues.schedule_time;
-              scheduleValues.schedule_unit = scheduleValues.scheduleHandler;
-              scheduleValues.schedule_time = moment(dt).format("HH:mm:ss");
-              break;
-
-            default:
-              break;
-          }
-
-          console.log(dataToSend);
-
-          return {
-            dataSeries: dataToSend,
-            schedule: scheduleValues,
-            filter: filterValues
-          };
-        };
-
-        // it dispatches post operation to nodejs
-        var _sendRequest = function(object) {
-          var request = null;
-          var data = {
-            dataSeries: object.dataToSend,
-            schedule: object.scheduleValues,
-            filter: object.filterValues,
-            service: object.serviceOutput,
-            intersection: object.intersection,
-            active: object.active
-          };
-          if ($scope.isUpdating) {
-            request = DataSeriesFactory.put(configuration.dataSeries.input.id, data);
-          } else {
-            request = DataSeriesFactory.post(data);
-          }
-
-          request.then(function(data) {
-            console.log(data);
-            $window.location.href = "/configuration/" + configuration.dataSeriesType + "/dataseries?token=" + (data.token || data.data.token);
-          }).catch(function(err) {
-            $scope.alertLevel = "alert-danger";
-            $scope.alertBox.message = err.message || err.data.message;
-            $scope.display = true;
-            $scope.extraProperties = {};
-            console.log(err);
-          });
-        };
-
         if ($scope.dataSeries.access == 'COLLECT') {
-          $scope.$on("storageValuesReceive", function(event, values) {
-            //  todo: improve
-            var dSemantics = Object.assign({}, $scope.dataSeries.semantics);
-            var dataObject = _save();
-            var dSeriesName = dataObject.dataSeries.name;
-            // setting _input in data series
-            dataObject.dataSeries.name += "_input";
-
-            var dSets = values.data;
-
-            // it makes data set format
-            var _makeFormat = function(dSetObject) {
-              var format_ = {};
-              for(var key in dSetObject) {
-                if (dSetObject.hasOwnProperty(key) && key.toLowerCase() !== "id")
-                  format_[key] = dSetObject[key];
-              }
-
-              // adding extra metadata
-              if (dSemantics.metadata.metadata && Object.keys(dSemantics.metadata.metadata).length > 0)
-                Object.assign(format_, dSemantics.metadata.metadata);
-
-              return format_;
-            };
-
-            var out;
-            if (dSets instanceof Array) {
-              // setting to active
-              var dSetsLocal = [];
-              dSets.forEach(function(dSet) {
-                dSetsLocal.push({
-                  active: $scope.dataSeries.active,
-                  format: _makeFormat(dSet)
-                });
-              });
-              out = dSetsLocal;
-            } else {
-              dSets.format = _makeFormat(dSets);
-              dSets.active = $scope.dataSeries.active;
-              out = [dSets];
-            }
-
-            // preparing intersection
-            var intersectionValues = [];
-            for(var k in $scope.intersection) {
-              if ($scope.intersection.hasOwnProperty(k)) {
-                if (!$scope.intersection[k].selected) {
-                  continue;
-                }
-                var attributes = $scope.intersection[k].attributes;
-                var dataseries_id = $scope.intersection[k].data_series.id;
-
-                // grid
-                if (attributes.length === 0) {
-                  intersectionValues.push({
-                    dataseries_id: dataseries_id
-                  });
-                }
-
-                for(var i = 0; i < attributes.length; ++i) {
-                  var attribute = attributes[i];
-                  intersectionValues.push({
-                    attribute: attribute,
-                    dataseries_id: dataseries_id
-                  });
-                }
-              }
-            }
-
-            var outputDataSeries = {
-              name: dSeriesName,
-              description: dataObject.dataSeries.description,
-              data_series_semantic_id: values.semantics.id,
-              data_provider_id: values.data_provider,
-              dataSets: out
-            };
-
-            _sendRequest({
-              dataToSend: {input: dataObject.dataSeries, output: outputDataSeries},
-              scheduleValues: dataObject.schedule,
-              filterValues: dataObject.filter,
-              serviceOutput: values.service,
-              intersection: intersectionValues,
-              active: dataObject.dataSeries.active
-            });
-
-          });
           // getting values from another controller
           $scope.$broadcast("requestStorageValues");
         } else {
