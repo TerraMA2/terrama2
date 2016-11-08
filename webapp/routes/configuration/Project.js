e443856487ae7b1758bede9379a13d79b2b471d0
Jean Souza <jeancfsouza@gmail.com>
2016-11-08 17:17:30 -0200

    redirecting to the project screen when it's registered

--- webapp/routes/configuration/Project.js
+++ webapp/routes/configuration/Project.js
@@ -6,5 +6,6 @@ module.exports = function (app) {
   app.get('/configuration/projects', passport.isAuthenticated, controller.get);
   app.get('/configuration/projects/new', passport.isAuthenticated, controller.new);
   app.get('/configuration/projects/:name/activate', passport.isAuthenticated, controller.activate);
+  app.get('/configuration/projects/:name/activate/:token', passport.isAuthenticated, controller.activate);
   app.get('/configuration/projects/edit/:name', passport.isAuthenticated, controller.edit);
 };
