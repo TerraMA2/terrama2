bb6dd185f37cbf1731f4869479364551115d1962
raphaelrpl <raphael.wcosta@gmail.com>
2016-11-10 10:00:58 -0200

    forcing folder format to string

--- webapp/core/data-model/DataSet.js
+++ webapp/core/data-model/DataSet.js
@@ -50,6 +50,9 @@ DataSet.prototype.setFormat = function(formats) {
  * @inherits AbstractData:toObject
  */
 DataSet.prototype.toObject = function() {
+  if (this.format && this.format.folder) {
+    this.format.folder = this.format.folder.toString();
+  }
   return Object.assign(AbstractClass.prototype.toObject.call(this), {
     id: this.id,
     data_series_id: this.data_series_id,
