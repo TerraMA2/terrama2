bab002af631de967603a0cf23afe9e9b3cff1d71
Jean Souza <jeancfsouza@gmail.com>
2016-12-07 17:17:30 -0200

    importing and exporting views

--- webapp/sockets/ImportExport.js
+++ webapp/sockets/ImportExport.js
@@ -356,6 +356,7 @@ var ImportExport = function(io) {
           output.DataSeries = [];
           output.Collectors = [];
           output.Analysis = [];
+          output.Views = [];
           var providers = DataManager.listDataProviders({project_id: projectId});
           providers.forEach(function(provider) {
             output.DataProviders.push(addID(provider));
