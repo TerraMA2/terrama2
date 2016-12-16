dc5dd38d3db1461c1286b50b4372220c008eae4f
Jean Souza <jeancfsouza@gmail.com>
2016-12-16 10:40:43 -0200

    saving the imported dcps

--- webapp/public/javascripts/angular/services.js
+++ webapp/public/javascripts/angular/services.js
@@ -263,6 +263,7 @@ angular.module("terrama2.services", ['terrama2'])
           var linesArray = reader.result.split("\n");
           var columns = linesArray[0].split(delimiterCharacter);
           var result = {
+            hasHeader: hasHeader,
             header: [],
             data: []
           };
