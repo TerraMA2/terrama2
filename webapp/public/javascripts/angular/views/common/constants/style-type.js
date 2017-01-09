a7e4f80d3db6c5f08fdc608db89c6f4c2717057c
raphaelrpl <raphael.wcosta@gmail.com>
2017-01-09 10:25:48 -0200

    adding initial view for vectorial data series

---
+++ webapp/public/javascripts/angular/views/common/constants/style-type.js
@@ -0,0 +1,21 @@
+define([], function() {
+  /**
+   * It determines the kind of style.
+   * 
+   * @enum {number}
+   */
+  return {
+    /**
+     * @example
+     */
+    "EQUAL_STEPS": 1,
+    /**
+     * @example
+     */
+    "QUANTILE": 2,
+    /**
+     * @example
+     */
+    "VALUE": 3
+  };
+});
--- webapp/public/javascripts/angular/views/common/index.js
+++ webapp/public/javascripts/angular/views/common/index.js
@@ -1,9 +1,11 @@
 define([
-  "TerraMA2WebApp/views/common/constants/style"
-], function(StyleConstants) {
+  "TerraMA2WebApp/views/common/constants/style",
+  "TerraMA2WebApp/views/common/constants/style-type"
+], function(StyleConstants, StyleType) {
   var moduleName = "terrama2.views.common";
 
   angular.module(moduleName, [])
+    .constant("StyleType", StyleType)
     .constant("StyleConstants", StyleConstants);
 
   return moduleName;
