eb6781e7ac0baf1db80a5ccd1f6fd3b4ea3e0072
janosimas <janosimas@gmail.com>
2017-03-07 09:59:37 -0300

    Find package for JWSMTP  and VMime libraries test code for vmime lib

--- src/examples/alert/Email.cpp
+++ src/examples/alert/Email.cpp
@@ -1,46 +1,92 @@
 #include <iostream>
 #include <jwsmtp.h>
 #include <cstring>
+#include <vmime.hpp>
 
 using std::cout;
 using std::cin;
 using std::string;
 
 void Usage( ) {
-  cout << "jwSMTP library demo program\n"
-      "maildemo \n"
-      " e.g.\n"
-      " maildemo you@there.com me@here.com mail.here.com\n";
+    cout << "jwSMTP library demo program\n"
+         "maildemo \n"
+         " e.g.\n"
+         " maildemo you@there.com me@here.com mail.here.com\n";
+}
+
+static vmime::shared_ptr <vmime::net::session> session = vmime::net::session::create();
+
+void vmimeTest()
+{
+    vmime::messageBuilder mb;
+    // Fill in the basic fields
+    mb.setExpeditor(vmime::mailbox("janosimas@gmail.com"));
+    vmime::addressList to;
+    to.appendAddress(vmime::make_shared <vmime::mailbox>("janosimas@gmail.com"));
+    mb.setRecipients(to);
+    vmime::addressList bcc;
+    bcc.appendAddress(vmime::make_shared <vmime::mailbox>("you-bcc@nowhere.com"));
+    mb.setBlindCopyRecipients(bcc);
+    mb.setSubject(vmime::text("My first message generated with vmime::messageBuilder"));
+    // Message body
+    mb.getTextPart()->setText(vmime::make_shared <vmime::stringContentHandler>(
+                                  "I'm writing this short text to test message construction " \
+                                  "using the vmime::messageBuilder component."));
+    // Construction
+    vmime::shared_ptr <vmime::message> msg = mb.construct();
+
+    // Raw text generation
+    std::cout << "Generated message:" << std::endl;
+    std::cout << "==================" << std::endl;
+
+    vmime::utility::outputStreamAdapter out(std::cout);
+    msg->generate(out);
+    std::cout << "\n\n" << std::endl;
+
+    vmime::utility::url url("smtp://smtp.gmail.com:587");
+    vmime::shared_ptr<vmime::net::transport> tr = session->getTransport(url);
+    tr->setProperty("connection.tls", true);
+    tr->setProperty("auth.username", "janosimas@gmail.com");
+    tr->setProperty("auth.password", "MinhaSenhaMuitoSegura!");
+    tr->setProperty("options.need-authentication", true);
+    tr->setCertificateVerifier(vmime::make_shared<vmime::security::cert::defaultCertificateVerifier >());
+    tr->connect();
+    tr->send(msg);
+
+    std::cout << "------------" << std::endl;
 }
 
 int main(int argc, char* argv[ ]) {
-  if(argc != 4) {
-    Usage( );
+
+    vmimeTest();
     return 0;
-  }
+    // if(argc != 4) {
+    //   Usage( );
+    //   return 0;
+    // }
 
-  cout << "jwSMTP library demo program\n\n";
-  string to = "paulo002@gmail.com";
-  string from = "paulo002@gmail.com";
-  string smtpserver = "smtp.gmail.com";
+    cout << "jwSMTP library demo program\n\n";
+    string to = "janosimas@gmail.com";
+    string from = "janosimas@gmail.com";
+    string smtpserver = "smtp.gmail.com";
 
-  std::string subject = "Test";
+    std::string subject = "Test";
 
-  string mailmessage = "This is a test message";
+    string mailmessage = "This is a test message";
 
-  // This is how to tell the mailer class that we are using a direct smtp server
-  // preventing the code from doing an MX lookup on 'smtpserver'
-  jwsmtp::mailer mail(to.c_str( ), from.c_str( ), subject.c_str( ), mailmessage.c_str( ),
-                      smtpserver.c_str( ), jwsmtp::mailer::SMTP_PORT, false);
+    // This is how to tell the mailer class that we are using a direct smtp server
+    // preventing the code from doing an MX lookup on 'smtpserver'
+    jwsmtp::mailer mail(to.c_str( ), from.c_str( ), subject.c_str( ), mailmessage.c_str( ),
+                        smtpserver.c_str( ), jwsmtp::mailer::SMTP_PORT, false);
 
-  // using a local file as opposed to a full path.
-  mail.attach("/home/paulo/Downloads/projeto_bugado.terrama2");
+    // using a local file as opposed to a full path.
+    // mail.attach("/home/paulo/Downloads/projeto_bugado.terrama2");
 
-  // boost::thread thrd(mail);
-  // thrd.join( ); // optional
-  // or:-
-  mail.operator ( )( );
-  cout << mail.response( ) << "\n";
+    // boost::thread thrd(mail);
+    // thrd.join( ); // optional
+    // or:-
+    mail.operator ( )( );
+    cout << mail.response( ) << "\n";
 
-  return 0;
+    return 0;
 }
