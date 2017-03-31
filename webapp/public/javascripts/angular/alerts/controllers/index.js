146892884405ff5878441d25cdfdd7a8b7ff7b10
franciscovn <franciscovn909@gmail.com>
2017-03-31 10:38:54 -0300

    Fix alerts list angular dependencies

--- webapp/public/javascripts/angular/alerts/controllers/index.js
+++ webapp/public/javascripts/angular/alerts/controllers/index.js
@@ -1,12 +1,10 @@
 define([
-  "TerraMA2WebApp/alerts/controllers/alert-list",
-  "TerraMA2WebApp/alerts/controllers/alert-register-update"
-], function(ListController, RegisterUpdateController){
+  "TerraMA2WebApp/alerts/controllers/alert-list"
+], function(ListController){
   var moduleName = "terrama2.alerts.controllers";
 
-  angular.module(moduleName, [ListController, RegisterUpdateController])
-    .controller("AlertListController", ListController)
-    .controller("AlertRegisterUpdateController", RegisterUpdateController);
+  angular.module(moduleName, [])
+    .controller("AlertListController", ListController);
 
   return moduleName;
 });
