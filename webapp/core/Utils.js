1eb57e1e1da36ce64b331ca32553ac8746419773
franciscovn <franciscovn909@gmail.com>
2017-04-06 10:26:17 -0300

    Prepare data provider object to send to service

--- webapp/core/Utils.js
+++ webapp/core/Utils.js
@@ -77,7 +77,7 @@ var Utils = module.exports = {
       key: UriPattern.PASSWORD,
       type: FormField.PASSWORD,
       htmlClass: 'col-md-6 terrama2-schema-form'
-    }];
+    }]';
   },
 
   makeCommonRequestFields: function(scheme, port, exceptField, required, displayOrder) {
@@ -222,7 +222,7 @@ var Utils = module.exports = {
       var dataProvidersResult = DataManager.listDataProviders();
       var providers = [];
       dataProvidersResult.forEach(function(dataProvider) {
-        providers.push(dataProvider.toObject());
+        providers.push(dataProvider.toService());
       }); // end foreach dataProvidersResult
 
       // getting dataseries
