8bc74782ddf57d20cb76eaef8245bc44d5ae6d68
Vinicius <vinicampa@gmail.com>
2017-04-07 09:23:01 -0300

    Document factory

---
+++ src/terrama2/services/alert/core/utility/DocumentFactory.cpp
@@ -0,0 +1,81 @@
+/*
+  Copyright (C) 2007 National Institute For Space Research (INPE) - Brazil.
+
+  This file is part of TerraMA2 - a free and open source computational
+  platform for analysis, monitoring, and alert of geo-environmental extremes.
+
+  TerraMA2 is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License,
+  or (at your option) any later version.
+
+  TerraMA2 is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with TerraMA2. See LICENSE. If not, write to
+  TerraMA2 Team at <terrama2-team@dpi.inpe.br>.
+*/
+
+/*!
+  \file terrama2/services/alert/core/utility/DocumentFactory.cpp
+
+  \brief
+
+  \author Jano Simas
+*/
+
+#include "DocumentFactory.hpp"
+#include "../Exception.hpp"
+#include "../../../../core/utility/Logger.hpp"
+
+#include <QString>
+#include <QObject>
+
+void terrama2::services::alert::core::DocumentFactory::add(const std::string& documentCode, FactoryFnctType f)
+{
+  auto it = factoriesMap_.find(documentCode);
+  if(it != factoriesMap_.end())
+  {
+    QString errMsg = QObject::tr("A document factory with this name already exists: %1.").arg(QString::fromStdString(documentCode));
+    TERRAMA2_LOG_ERROR() << errMsg.toStdString();
+    throw terrama2::services::alert::NotifierException() << ErrorDescription(errMsg);
+  }
+
+  factoriesMap_[documentCode] = f;
+}
+
+void terrama2::services::alert::core::DocumentFactory::remove(const std::string& documentCode)
+{
+  std::map<std::string, FactoryFnctType>::const_iterator it = factoriesMap_.find(documentCode);
+  if(it == factoriesMap_.end())
+  {
+    QString errMsg = QObject::tr("There is no registered document factory named : %1.").arg(QString::fromStdString(documentCode));
+    TERRAMA2_LOG_ERROR() << errMsg.toStdString();
+    throw terrama2::services::alert::NotifierException() << ErrorDescription(errMsg);
+  }
+
+  factoriesMap_.erase(it);
+}
+
+bool terrama2::services::alert::core::DocumentFactory::find(const std::string& documentCode)
+{
+  std::map<std::string, FactoryFnctType>::const_iterator it = factoriesMap_.find(documentCode);
+  return (it != factoriesMap_.end());
+}
+
+std::string terrama2::services::alert::core::DocumentFactory::makeDocument(const std::string& documentCode,
+                                                                           ReportPtr report)
+{
+  std::map<std::string, FactoryFnctType>::const_iterator it = factoriesMap_.find(documentCode);
+  if(it == factoriesMap_.end())
+  {
+    QString errMsg = QObject::tr("Could not find a document factory for this code: %1.").arg(QString::fromStdString(documentCode));
+    TERRAMA2_LOG_ERROR() << errMsg.toStdString();
+    throw terrama2::services::alert::NotifierException() << ErrorDescription(errMsg);
+  }
+
+  return factoriesMap_[documentCode](report);
+}
---
+++ src/terrama2/services/alert/core/utility/DocumentFactory.hpp
@@ -0,0 +1,93 @@
+/*
+  Copyright (C) 2007 National Institute For Space Research (INPE) - Brazil.
+
+  This file is part of TerraMA2 - a free and open source computational
+  platform for analysis, monitoring, and alert of geo-environmental extremes.
+
+  TerraMA2 is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License,
+  or (at your option) any later version.
+
+  TerraMA2 is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with TerraMA2. See LICENSE. If not, write to
+  TerraMA2 Team at <terrama2-team@dpi.inpe.br>.
+*/
+
+/*!
+  \file terrama2/services/alert/core/utility/DocumentFactory.hpp
+
+  \brief
+
+  \author Vinicius Campanha
+*/
+
+#ifndef __TERRAMA2_SERVICES_ALERT_CORE_DOCUMENT_FACTORY_HPP__
+#define __TERRAMA2_SERVICES_ALERT_CORE_DOCUMENT_FACTORY_HPP__
+
+#include "../Shared.hpp"
+
+// TerraLib
+#include <terralib/common/Singleton.h>
+
+// STL
+#include <functional>
+#include <map>
+
+namespace terrama2
+{
+  namespace services
+  {
+    namespace alert
+    {
+      namespace core
+      {
+        class DocumentFactory : public te::common::Singleton<DocumentFactory>
+        {
+          public:
+            //! DataAccessor constructor function.
+            typedef std::function<std::string (ReportPtr report)> FactoryFnctType;
+            //! Register a new DataAccessor constructor associated with the DataSeriesSemantics.
+            void add(const std::string& documentCode, FactoryFnctType f);
+            //! Remove the DataAccessor constructor associated with the DataSeriesSemantics.
+            void remove(const std::string& documentCode);
+            //! Check if there is a DataAccessor constructor associated with the DataSeriesSemantics.
+            bool find(const std::string& documentCode);
+
+            /*!
+              * \brief makeDocument
+              * \param documentCode
+              * \param alert
+              * \param report
+              * \return
+              */
+             std::string makeDocument(const std::string& documentCode, ReportPtr report);
+
+          protected:
+            friend class te::common::Singleton<DocumentFactory>;
+
+            //! Default constructor.
+            DocumentFactory() = default;
+            //! Default destructor
+            virtual ~DocumentFactory() = default;
+
+            DocumentFactory(const DocumentFactory& other) = delete;
+            DocumentFactory(DocumentFactory&& other) = delete;
+            DocumentFactory& operator=(const DocumentFactory& other) = delete;
+            DocumentFactory& operator=(DocumentFactory&& other) = delete;
+
+          private:
+
+            std::map<std::string, FactoryFnctType> factoriesMap_;
+        };
+      } /* core */
+    } /* alert */
+  } /* services */
+} /* terrama2 */
+
+#endif //__TERRAMA2_SERVICES_ALERT_CORE_DOCUMENT_FACTORY_HPP__
