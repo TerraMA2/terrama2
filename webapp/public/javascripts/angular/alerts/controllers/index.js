3154e75424115bd5f5984425365b85334c9f94d6
franciscovn <franciscovn909@gmail.com>
2017-06-06 15:18:17 -0300

    Adding schedule options on create/update alert

--- webapp/public/javascripts/angular/alerts/controllers/index.js
+++ webapp/public/javascripts/angular/alerts/controllers/index.js
@@ -2,16 +2,20 @@ define([
   "TerraMA2WebApp/common/loader",
   "TerraMA2WebApp/common/app",
   "TerraMA2WebApp/alert-box/app",
+  "TerraMA2WebApp/data-series/schedule",
   "TerraMA2WebApp/data-series/services",
+  "TerraMA2WebApp/datetimepicker/directive",
   "TerraMA2WebApp/services/services",
   "TerraMA2WebApp/alerts/controllers/alert-list",
   "TerraMA2WebApp/alerts/controllers/alert-register-update"
-], function(moduleLoader, commonApp, messageBoxApp, dataSeriesServiceApp, serviceApp, ListController, RegisterUpdateController){
+], function(moduleLoader, commonApp, messageBoxApp, scheduleApp, dataSeriesServiceApp, datetimepicker, serviceApp, ListController, RegisterUpdateController){
   var moduleName = "terrama2.alerts.controllers";
 
   var deps = [messageBoxApp];
   moduleLoader("ui.select", deps);
   moduleLoader(dataSeriesServiceApp, deps);
+  moduleLoader(datetimepicker, deps);
+  moduleLoader(scheduleApp, deps);
   moduleLoader(serviceApp, deps);
 
   angular.module(moduleName, deps)
