cafcf5a745a4bab6706fc3c731377a976f260892
janosimas <janosimas@gmail.com>
2017-06-08 14:34:09 -0300

    preparing to store DCP geometry

--- src/terrama2/impl/DataStoragerDCPPostGIS.cpp
+++ src/terrama2/impl/DataStoragerDCPPostGIS.cpp
@@ -30,10 +30,38 @@
 //TerraMA2
 #include "DataStoragerDCPPostGIS.hpp"
 
+#include "../core/data-model/DataProvider.hpp"
+#include "../core/utility/Raii.hpp"
+
+#include <terralib/dataaccess/datasource/DataSourceTransactor.h>
+#include <terralib/dataaccess/datasource/ScopedTransaction.h>
+#include <terralib/dataaccess/datasource/DataSourceFactory.h>
+#include <terralib/dataaccess/datasource/DataSource.h>
+#include <terralib/core/uri/URI.h>
+
+//Boost
+#include <boost/algorithm/string.hpp>
+
 void terrama2::core::DataStoragerDCPPostGIS::store(const std::unordered_map<DataSetPtr,DataSetSeries >&  dataMap,
                                                    const std::vector< DataSetPtr >& dataSetLst,
                                                    const std::map<DataSetId, DataSetId>& inputOutputMap) const
 {
+
+  { //connection scope
+    te::core::URI uri(dataProvider_->uri);
+    std::shared_ptr<te::da::DataSource> datasourceDestination(te::da::DataSourceFactory::make(driver(), uri));
+
+    OpenClose< std::shared_ptr<te::da::DataSource> > openClose(datasourceDestination); Q_UNUSED(openClose);
+
+    for(const auto& item : dataMap)
+    {
+      // read each item
+      DataSetId outputDataSetId = inputOutputMap.at(item.first->id);
+      auto outputDataSet = std::find_if(dataSetLst.cbegin(), dataSetLst.cend(), [outputDataSetId](terrama2::core::DataSetPtr dataSet) { return dataSet->id == outputDataSetId; });
+      std::string destinationDataSetName = boost::to_lower_copy(getDataSetName(*outputDataSet));
+    }
+
+  }// end of the connection scope
 
   DataStorager::store(dataMap, dataSetLst, inputOutputMap);
 }
