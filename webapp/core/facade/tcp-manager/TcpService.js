6517baf4b6cf0a7ccfca4e594f5db68f520ae2a0
franciscovn <franciscovn909@gmail.com>
2017-06-23 15:15:01 -0300

    Behavior to start view alert

--- webapp/core/facade/tcp-manager/TcpService.js
+++ webapp/core/facade/tcp-manager/TcpService.js
@@ -297,10 +297,38 @@ TcpService.prototype.run = function(processObject) {
     delete processObject.service_instance;
     return DataManager.getServiceInstance({id: service})
       .then(function(instance) {
-        // Forcing a process to run
-        TcpManager.startProcess(instance, processObject);
-        // Retrieving log status of process (STARTED/ON_QUEUED, etc)
-        TcpManager.logData(instance, {begin: 0, end: 2, process_ids: processObject.ids});
+        // If alert service, check if have a view to start first
+        if (instance.service_type_id == ServiceType.ALERT){
+          return DataManager.getAlert({id: processObject.ids[0]}).then(function(alert){
+            // if alert has view, start the view process
+            if (alert.view && alert.view.id){
+              return DataManager.getView({id: alert.view.id}).then(function(view){
+                return DataManager.getServiceInstance({id: view.serviceInstanceId}).then(function(view_instance){
+                  var viewProcessObject = {
+                    execution_date: processObject.execution_date,
+                    ids: [view.id]
+                  };
+                  startProcess(view_instance, viewProcessObject);
+                  // Notify children listeners the process has been scheduled
+                  self.emit("processRun", viewProcessObject);
+                  return resolve();
+                })
+              });
+            // else start alert process normally
+            } else {
+              startProcess(instance, processObject);
+              // Notify children listeners the process has been scheduled
+              self.emit("processRun", processObject);
+              return resolve();
+            }
+          })
+          .catch(function(err) {
+            logger.debug(err);
+            return reject(err);
+          });
+        }
+        
+        startProcess(instance, processObject);
         // Notify children listeners the process has been scheduled
         self.emit("processRun", processObject);
         return resolve();
@@ -686,9 +714,37 @@ function onProcessFinished(resp) {
   // broadcast to everyone
   if (resp && resp instanceof RegisteredView) {
     tcpService.emit("viewReceived", resp);
+    // check if is a view from alert to execute the alert process
+    DataManager.getAlert({view_id: resp.view.id})
+      .then(function(alert){
+        DataManager.getServiceInstance({id: alert.service_instance_id}).then(function(instance){
+          var alertProcessObject = {
+            ids: [alert.id],
+            execution_date: new Date().toISOString()
+          };
+          startProcess(instance, alertProcessObject);
+          // Notify children listeners the process has been scheduled
+          tcpService.emit("processRun", alertProcessObject);
+        })
+      })
+      .catch(function(){
+        logger.debug("Dont have alert of the view");
+      });
   } 
   // Notifies that process finished
   tcpService.emit("processFinished", resp.process);
+}
+
+/**
+ * Start process
+ * @param {*} instance 
+ * @param {*} processObject 
+ */
+function startProcess(instance, processObject){
+  // Forcing a process to run
+  TcpManager.startProcess(instance, processObject);
+  // Retrieving log status of process (STARTED/ON_QUEUED, etc)
+  TcpManager.logData(instance, {begin: 0, end: 2, process_ids: processObject.ids});
 }
 
 /**
