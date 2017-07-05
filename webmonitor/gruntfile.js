78e307417df5f3567420e20aeb22093b82eef693
Jean Souza <jeancfsouza@gmail.com>
2017-07-05 14:10:14 -0300

    starting the refactoring of the webmonitor

---
+++ webmonitor/gruntfile.js
@@ -0,0 +1,98 @@
+module.exports = function(grunt) {
+
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> <%= pkg.version %> | (C) 2007, <%= grunt.template.today("yyyy") %> National Institute For Space Research (INPE) - Brazil | https://github.com/TerraMA2/terrama2/blob/master/LICENSE */',
+    requirejs: {
+      TerraMA2WebMonitor: {
+        options: {
+          baseUrl: "public/javascripts",
+          out: "public/dist/TerraMA2WebMonitor.min.js",
+          preserveLicenseComments: false,
+          paths: {
+            TerraMA2WebComponents: "../../../webcomponents/dist/TerraMA2WebComponents.min"
+          },
+          include: [
+            '../../bower_components/almond/almond',
+            'Startup',
+            'TerraMA2WebMonitor'
+          ]
+        }
+      }
+    },
+    cssmin: {
+      TerraMA2WebMonitor: {
+        files: [{
+          expand: true,
+          cwd: 'public/stylesheets',
+          src: ['*.css'],
+          dest: 'public/dist',
+          ext: '.min.css'
+        }]
+      }
+    },
+    usebanner: {
+      TerraMA2WebMonitor: {
+        options: {
+          position: 'top',
+          banner: '<%= banner %>',
+          linebreak: true
+        },
+        files: {
+          src: [ 'public/dist/*' ]
+        }
+      }
+    },
+    jsdoc : {
+      TerraMA2WebMonitor: {
+        src: ['README.md'],
+        options: {
+          configure: 'config/jsdoc/config.json',
+          destination: 'public/docs'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ["public/stylesheets/*.css"],
+        tasks: ["cssmin", "usebanner"],
+        options: {
+          // It allows to compile only when needed
+          spawn: false
+        }
+      },
+      gruntfile: {
+        files: ["gruntfile.js"],
+        tasks: ["default"],
+        options: {
+          spawn: false
+        }
+      },
+      js: {
+        files: ["public/javascripts/*.js", "public/javascripts/components/*.js"],
+        tasks: ["requirejs", "usebanner"],
+        options: {
+          // It allows to compile only when needed
+          spawn: false
+        }
+      }
+    }
+  });
+
+  // Print helper to detect which file has beed changed
+  grunt.event.on('watch', function(action, filepath, target) {
+    grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+  });
+
+  // Load the plugins.
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-banner');
+  grunt.loadNpmTasks('grunt-jsdoc');
+
+  // Default tasks.
+  grunt.registerTask('default', ['requirejs', 'cssmin', 'usebanner:TerraMA2WebMonitor', 'jsdoc']);
+
+};
---
+++ webmonitor/public/javascripts/components/Calendar.js
@@ -0,0 +1,146 @@
+'use strict';
+
+define(
+  ['TerraMA2WebComponents'],
+  function(TerraMA2WebComponents) {
+
+    /**
+     * It makes a input hidden with date limits (startDate & endDate)
+     * 
+     * @returns {string}
+     */
+    var makeHelperDatePicker = function(capability) {
+      if (!capability) {
+        throw new Error("Invalid capability while making date picker");
+      }
+      /**
+       * It represents a input hidden to store min date and max date
+       * @type {string}
+       */
+      var datepicker = "<input type='hidden' name='" + capability.name + "'";
+
+      if (capability.extent instanceof Array) {
+        datepicker += " data-min-date='" + capability.extent[0] + "' data-max-date='" + capability.extent[capability.extent.length - 1] + "'>";
+      } else if (capability.extent instanceof Object) {
+        datepicker += " data-min-date='" + capability.extent.startDate + "' data-max-date='" + capability.extent.endDate + "' start-date='' end-date=''" +">";
+      }
+
+      return datepicker;
+    };
+
+    /**
+     * It updates the calendar start, end date
+     * @param {object} capability - object with dates 
+     * @param {string} layerId - layer id to update
+     */
+    var updateDatePicker = function(capability, layerId){
+      var listElement = $("li[data-layerid='"+ layerId +"']");
+      var layerName = layerId.split(":")[1];
+      var calendar = listElement.find("input[name='" + layerName +"']");
+
+      if (capability.extent instanceof Array) {
+        calendar.attr("data-min-date", capability.extent[0]);
+        calendar.attr("data-max-date", capability.extent[capability.extent.length - 1]);
+      } else if (capability.extent instanceof Object) {
+        calendar.attr("data-min-date", capability.extent.startDate);
+        calendar.attr("data-max-date", capability.extent.endDate);
+      }
+    };
+
+    var init = function() {
+      $("#terrama2-layerexplorer").on("click", "#terrama2-calendar", function(event) {
+        var self = $(this);
+        var parentLi = $(self).parent();
+        var capability = $(self).attr("data-layerid");
+        var calendar = $(self).find("input[id='"+ capability +"']");
+        var hidden = $(self).find("input[type='hidden']");
+        var minDate = $(hidden).attr('data-min-date');
+        var maxDate = $(hidden).attr('data-max-date');
+        var startDate = $(hidden).attr('start-date');
+        var endDate = $(hidden).attr('end-date');
+
+        if (!minDate || !maxDate) {
+          return;
+        }
+        var mMinDate = moment(minDate);
+        var mMaxDate = moment(maxDate);
+        var mStartDate;
+        var mEndDate;
+        if (!startDate || !endDate){
+          mStartDate = mMaxDate;
+          mEndDate = mMaxDate;
+        } else {
+          mStartDate = moment(startDate);
+          mEndDate = moment(endDate);
+        }
+        if (calendar.length === 0) {
+          calendar = $("<input type='text' id='" + capability + "' value='' style='display:none;'>");
+          /**
+           * Important: Due date range picker implementation, we must configure startDate and endDate. Otherwise, it will display NaN
+           * date values because the start date is None, so the component take on current timestamp.
+           */
+          $(calendar).daterangepicker({
+            "timePicker": true,
+            "minDate": mMinDate,
+            "startDate": mStartDate,
+            "endDate": mEndDate,
+            "maxDate": mMaxDate,
+            "timePicker24Hour": true,
+            "opens": "center"
+          });
+
+          $(this).append(calendar);
+
+          $(calendar).on('click', function(e) {
+            e.stopPropagation();
+          })
+
+          $(calendar).on("apply.daterangepicker", function(ev, picker) {
+            //  DO REQUEST
+            var layerId = $(parentLi).attr("data-layerid");
+            var timeFormat = "YYYY-MM-DDTHH:mm:ss";
+            var pickerStartDate = picker.startDate.format(timeFormat);
+            var pickerEndDate = picker.endDate.format(timeFormat);
+            $(hidden).attr('start-date', pickerStartDate);
+            $(hidden).attr('end-date', pickerEndDate);
+
+            var layerTime = pickerStartDate + "Z/" + pickerEndDate + "Z";
+            TerraMA2WebComponents.MapDisplay.updateLayerTime(/**id */layerId, /** time */layerTime);
+          });
+        } else {
+
+          $(calendar).daterangepicker({
+            "timePicker": true,
+            "minDate": mMinDate,
+            "startDate": mStartDate,
+            "endDate": mEndDate,
+            "maxDate": mMaxDate,
+            "timePicker24Hour": true,
+            "opens": "center"
+          });
+
+          $(calendar).on("apply.daterangepicker", function(ev, picker) {
+            //  DO REQUEST
+            var layerId = $(parentLi).attr("data-layerid");
+            var timeFormat = "YYYY-MM-DDTHH:mm:ss";
+            var pickerStartDate = picker.startDate.format(timeFormat);
+            var pickerEndDate = picker.endDate.format(timeFormat);
+            $(hidden).attr('start-date', pickerStartDate);
+            $(hidden).attr('end-date', pickerEndDate);
+
+            var layerTime = pickerStartDate + "Z/" + pickerEndDate + "Z";
+            TerraMA2WebComponents.MapDisplay.updateLayerTime(/**id */layerId, /** time */layerTime);
+          });
+        }
+
+        $(calendar).trigger('click');
+      });
+    };
+
+    return {
+      makeHelperDatePicker: makeHelperDatePicker,
+      updateDatePicker: updateDatePicker,
+      init: init
+    };
+  }
+);
