1e040f98b766009e59f2151b8a5b5062c9a4c045
Jean Souza <jeancfsouza@gmail.com>
2017-07-10 10:06:01 -0300

    creating http retriever

---
+++ src/examples/core/DataRetrieverFromHTTP.cpp
@@ -0,0 +1,70 @@
+
+// TerraMA2
+#include <terrama2/core/Shared.hpp>
+#include <terrama2/impl/DataRetrieverHTTP.hpp>
+#include <terrama2/core/utility/Utils.hpp>
+#include <terrama2/core/data-model/DataProvider.hpp>
+#include <terrama2/core/data-model/Filter.hpp>
+#include <terrama2/core/utility/Raii.hpp>
+#include <terrama2/core/utility/FilterUtils.hpp>
+
+// STL
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <functional>
+
+// QT
+#include <QUrl>
+#include <QObject>
+#include <QDir>
+#include <QFile>
+#include <QDebug>
+
+// Libcurl
+#include <curl/curl.h>
+
+
+
+int main(int argc, char* argv[])
+{
+  QUrl url("https://prodwww-queimadas.dgi.inpe.br/sld/");
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  std::string path;
+  std::string mask = "focos_novos_ql.sld";
+
+  auto remover = std::make_shared<terrama2::core::FileRemover>();
+  {
+    //DataProvider information
+    terrama2::core::DataProvider* dataProvider = new terrama2::core::DataProvider();
+
+    std::unique_ptr<te::ws::core::CurlWrapper> curlwrapper(new te::ws::core::CurlWrapper());
+    terrama2::core::DataProviderPtr dataProviderPtr(dataProvider);
+    dataProvider->uri = url.url().toStdString();
+    dataProvider->intent = terrama2::core::DataProviderIntent::COLLECTOR_INTENT;
+    dataProvider->dataProviderType = "HTTP";
+    dataProvider->active = true;
+
+    //empty filter
+    terrama2::core::Filter filter;
+    //accessing data
+    terrama2::core::DataRetrieverHTTP retrieverHTTP(dataProviderPtr, std::move(curlwrapper));
+
+    path = retrieverHTTP.retrieveData(mask, filter, "UTC+00", remover);
+  }
+
+  curl_global_cleanup();
+
+  QUrl uriLocal(path.c_str());
+  path = uriLocal.path().toStdString() +"/"+ mask;
+  QFile file(path.c_str());
+  // Check if the file exists before deleting the folder.
+  if (file.exists())
+    qDebug() << "Download complete!";
+  else
+    qDebug() << "Test failed!";
+
+  return 0;
+}
---
+++ src/terrama2/impl/DataRetrieverHTTP.cpp
@@ -0,0 +1,274 @@
+/*
+  Copyright (C) 2007 National Institute For Space Research (INPE) - Brazil.
+
+  This file is part of TerraMA2 - a free and open source computational
+  platform for analysis, monitoring, and alert of geo-environmental extremes.
+
+  TerraMA2 is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 3 of the License,
+  or (at your option) any later version.
+
+  TerraMA2 is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with TerraMA2. See LICENSE. If not, write to
+  TerraMA2 Team at <terrama2-team@dpi.inpe.br>.
+*/
+
+/*!
+  \file terrama2/impl/DataRetrieverHTTP.cpp
+
+  \brief Data Retriever HTTP.
+
+  \author Jean Souza
+*/
+
+// STL
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <functional>
+
+// TerraMA2
+#include "DataRetrieverHTTP.hpp"
+#include "../core/data-model/Filter.hpp"
+#include "../core/utility/Raii.hpp"
+#include "../core/utility/Logger.hpp"
+#include "../core/data-model/Filter.hpp"
+#include "../core/utility/FilterUtils.hpp"
+#include "../core/utility/Utils.hpp"
+
+// TerraLib
+#include <terralib/core/uri/URI.h>
+#include <terralib/core/Exception.h>
+
+// Libcurl
+#include <curl/curl.h>
+
+// Boost
+#include <boost/algorithm/string.hpp>
+#include <boost/filesystem/operations.hpp>
+
+// QT
+#include <QObject>
+#include <QDir>
+#include <QDebug>
+#include <QUrl>
+
+terrama2::core::DataRetrieverHTTP::DataRetrieverHTTP(DataProviderPtr dataprovider, std::unique_ptr<te::ws::core::CurlWrapper>&& curlwrapper)
+  : DataRetriever(dataprovider),
+    curlwrapper_(std::move(curlwrapper))
+{
+  // Verifies if the HTTP address is valid
+  try
+  {
+    curlwrapper_->verifyURL(dataprovider->uri, dataProvider_->timeout);
+  }
+  catch(const te::Exception& e)
+  {
+    QString errMsg = QObject::tr("HTTP address is invalid! \n\n Details: \n");
+    auto errStr = boost::get_error_info<te::ErrorDescription>(e);
+    if(errStr)
+      errMsg.append(QString::fromStdString(*errStr));
+    errMsg.append(e.what());
+
+    TERRAMA2_LOG_ERROR() << errMsg;
+    throw DataRetrieverException() << ErrorDescription(errMsg);
+  }
+  catch(...)
+  {
+    throw DataRetrieverException() << ErrorDescription(QObject::tr("Unknown Error, HTTP address is invalid!"));
+  }
+
+}
+
+bool terrama2::core::DataRetrieverHTTP::isRetrivable() const noexcept
+{
+  return true;
+}
+
+terrama2::core::DataRetrieverHTTP::~DataRetrieverHTTP()
+{
+
+}
+
+std::vector<std::string> terrama2::core::DataRetrieverHTTP::getFoldersList(const std::vector<std::string>& uris,
+                                                                          const std::string& foldersMask)
+{
+  std::vector<std::string> maskList = splitString(foldersMask, '/');
+
+  if(maskList.empty())
+    return uris;
+
+  std::vector<std::string> folders = uris;
+
+  for(const auto& mask : maskList)
+  {
+    if(!mask.empty())
+      folders = checkSubfolders(folders, mask);
+  }
+
+  if(folders.empty())
+  {
+    QString errMsg = QObject::tr("No directory matches the mask.");
+    TERRAMA2_LOG_ERROR() << errMsg;
+    return {};
+  }
+
+  return folders;
+}
+
+std::vector<std::string> terrama2::core::DataRetrieverHTTP::checkSubfolders(const std::vector<std::string> baseURIs, const std::string mask)
+{
+  std::vector<std::string> folders;
+
+  for(const auto& uri : baseURIs)
+  {
+    std::vector<std::string> dirList = curlwrapper_->listFiles(te::core::URI(uri));
+
+    if(dirList.empty())
+    {
+      continue;
+    }
+
+    for(const auto& folder : dirList)
+    {
+      if(!terramaMaskMatch(mask, folder))
+        continue;
+
+      folders.push_back(uri + "/" + folder + "/");
+    }
+  }
+
+  return folders;
+}
+
+
+std::string terrama2::core::DataRetrieverHTTP::retrieveData(const std::string& mask,
+                                                           const Filter& filter,
+                                                           const std::string& timezone,
+                                                           std::shared_ptr<terrama2::core::FileRemover> remover,
+                                                           const std::string& temporaryFolderUri,
+                                                           const std::string& foldersMask)
+{
+  std::string downloadBaseFolderUri = temporaryFolderUri + "/";
+
+  if(temporaryFolderUri.empty())
+  {
+    boost::filesystem::path tempDir = boost::filesystem::temp_directory_path();
+    boost::filesystem::path tempTerrama(tempDir.string()+"/terrama2");
+    boost::filesystem::path downloadBaseDir = boost::filesystem::unique_path(tempTerrama.string()+"/%%%%-%%%%-%%%%-%%%%");
+
+    // Create the directory where you will download the files.
+    QDir dir(QString::fromStdString(downloadBaseDir.string()));
+    if(!dir.exists())
+      dir.mkpath(QString::fromStdString(downloadBaseDir.string()));
+
+    std::string scheme = "file://";
+    downloadBaseFolderUri = scheme + downloadBaseDir.string();
+    remover->addTemporaryFolder(downloadBaseFolderUri);
+
+    downloadBaseFolderUri = scheme + downloadBaseDir.string();
+  }
+
+  try
+  {
+    // find valid directories
+    std::vector< std::string > baseUriList;
+    baseUriList.push_back(dataProvider_->uri);
+
+    if(!foldersMask.empty())
+    {
+      auto uriList = getFoldersList(baseUriList, foldersMask);
+
+      if(uriList.empty())
+      {
+        QString errMsg = QObject::tr("No files found!");
+        TERRAMA2_LOG_WARNING() << errMsg;
+        throw terrama2::core::NoDataException() << ErrorDescription(errMsg);
+      }
+
+      baseUriList = uriList;
+    }
+
+    // Get a file listing from server
+    for(const auto& uri : baseUriList)
+    {
+      // Create directory struct
+      QString saveDir(QString::fromStdString(uri));
+      saveDir.replace(QString::fromStdString(dataProvider_->uri), QString::fromStdString(downloadBaseFolderUri));
+
+      QString savePath = QUrl(saveDir).toString(QUrl::RemoveScheme);
+      QDir dir(savePath);
+      if(!dir.exists())
+        dir.mkpath(savePath);
+
+      // Performs the download of files in the vectorNames
+      //for(const auto& file: vectorNames)
+      //{
+        std::string uriOrigin = uri + "/" + mask;
+        std::string filePath = savePath.toStdString() + "/" + mask;
+
+        try
+        {
+          curlwrapper_->downloadFile(uriOrigin, filePath);
+        }
+        catch(const te::Exception& e)
+        {
+          QString errMsg = QObject::tr("Error during download of file %1.\n").arg(QString::fromStdString(mask));
+          auto errStr = boost::get_error_info<te::ErrorDescription>(e);
+          if(errStr)
+            errMsg.append(QString::fromStdString(*errStr));
+          errMsg.append(e.what());
+
+          TERRAMA2_LOG_ERROR() << errMsg;
+          throw DataRetrieverException() << ErrorDescription(errMsg);
+        }
+
+        remover->addTemporaryFile(filePath);
+      //}
+    }
+  }
+  catch(const NoDataException&)
+  {
+    throw;
+  }
+  catch(const DataRetrieverException&)
+  {
+    throw;
+  }
+  catch(const te::Exception& e)
+  {
+    QString errMsg = QObject::tr("Error during download.\n");
+    errMsg.append(boost::get_error_info<terrama2::ErrorDescription>(e));
+    errMsg.append(e.what());
+
+    TERRAMA2_LOG_ERROR() << errMsg;
+    throw DataRetrieverException() << ErrorDescription(errMsg);
+  }
+  catch(const std::exception& e)
+  {
+    QString errMsg = QObject::tr("Error during download.\n");
+    errMsg.append(e.what());
+
+    TERRAMA2_LOG_ERROR() << errMsg;
+    throw DataRetrieverException() << ErrorDescription(errMsg);
+  }
+  catch(...)
+  {
+    throw DataRetrieverException() << ErrorDescription(QObject::tr("Unknown Error."));
+  }
+
+  // returns the absolute path of the folder that contains the files that have been made the download.
+  return downloadBaseFolderUri;
+}
+
+terrama2::core::DataRetrieverPtr terrama2::core::DataRetrieverHTTP::make(DataProviderPtr dataProvider)
+{
+  std::unique_ptr<te::ws::core::CurlWrapper> curlwrapper(new te::ws::core::CurlWrapper());
+  return std::make_shared<DataRetrieverHTTP>(dataProvider, std::move(curlwrapper));
+}
