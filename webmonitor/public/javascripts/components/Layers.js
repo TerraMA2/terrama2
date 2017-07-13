8828d5999f8d80a94252d00d0f02220dcab97850
franciscovn <franciscovn909@gmail.com>
2017-07-13 09:42:20 -0300

    [Partial] change project

--- webmonitor/public/javascripts/components/Layers.js
+++ webmonitor/public/javascripts/components/Layers.js
@@ -14,14 +14,25 @@ define(
       memberAllLayers.push(layerObject);
     }
 
+    var getLayerById = function(layerId){
+      var indexLayer = memberAllLayers.map(function(l){return l.id}).indexOf(layerId);
+      if( indexLayer > 0)
+        return memberAllLayers[indexLayer];
+      else 
+        return null;
+      
+    }
+
     var createLayerObject = function (layerData){
       var layerObject = {};
       layerObject.name = layerData.name;
+      layerObject.nameId = layerData.layers[0];
       layerObject.workspace = layerData.workspace;
       layerObject.id = layerData.workspace ? layerData.workspace + ":" + layerData.layers[0] : layerData.layers[0];
       layerObject.htmlId = layerObject.id.replace(":", "").split('.').join('\\.');
       layerObject.uriGeoServer = layerData.uriGeoserver;
-      layerObject.parent = layerData.serverType;
+      layerObject.parent = layerData.type;
+      layerObject.serverType = layerData.serverType;
       layerObject.isParent = layerData.serverType ? false : true;
       layerObject.projectId = layerData.projectId;
       layerObject.private = layerData.private;
@@ -38,43 +49,48 @@ define(
       var allLayers = getAllLayers();
       var allLayersLength = allLayers.length;
       for (var i = allLayersLength -1; i >= 0; i--){
-        var layerId = allLayers[i].id;
-        var htmlId = allLayers[i].htmlId;
+        if (!allLayers[i].projectId){
+          var layerId = allLayers[i].id;
+          var htmlId = allLayers[i].htmlId;
 
-        var sliderDiv = "<div class='slider-content' style='display:none;'><label></label><button type='button' class='close close-slider'>×</button><div id='opacity" + htmlId + "'></div></div>";
-        var spanIcon = "<span id='terrama2-opacity-slider' class='terrama2-datepicker-icon' data-toggle='tooltip' title='Change opacity'> <i class='fa fa-sliders'></i></span>"; 
-        
-        itens += '<li id="' + htmlId + '" data-layerid="' + layerId + '" data-parentid="terrama2-layerexplorer" class="hide">' + allLayers[i].name + sliderDiv + spanIcon + '</li>';
+          var sliderDiv = "<div class='slider-content' style='display:none;'><label></label><button type='button' class='close close-slider'>×</button><div id='opacity" + htmlId + "'></div></div>";
+          var spanIcon = "<span id='terrama2-opacity-slider' class='terrama2-datepicker-icon' data-toggle='tooltip' title='Change opacity'> <i class='fa fa-sliders'></i></span>"; 
+          
+          itens += '<li id="' + htmlId + '" data-layerid="' + layerId + '" data-parentid="terrama2-layerexplorer" class="hide">' + allLayers[i].name + sliderDiv + spanIcon + '</li>';
+        }
       }
       var list = '<ul>' + itens + '</ul>';
       $('#terrama2-sortlayers').append(list);
     };
 
     // Add layers in layers explorer menu
-    var fillLayersData = function(data) {
+    var fillLayersData = function(layers) {
+      var data = layers;
+      if (!data){
+        data = memberAllLayers;
+      }
+      var currentProject = $("#projects").val();
 			for(var i in data) {
-				if(!data[i].private || (data[i].private && userLogged)) {
+				if((!data[i].private || (data[i].private && userLogged)) && data[i].projectId && data[i].projectId == currentProject) {
           var workspace = data[i].workspace;
           var layerName = data[i].name;
-          var uriGeoServer = data[i].uriGeoserver;
+          var uriGeoServer = data[i].uriGeoServer;
           var serverType = data[i].serverType;
-          var layerId = workspace + ":" + data[i].layers[0];
-          Utils.getSocket().emit('checkConnection', {url: uriGeoServer, requestId: layerId});
-          if (memberAllLayers.map(function (l){return l.id}).indexOf(layerId) > 0)
-            continue;
+          var parent = data[i].parent;
+          var layerId = data[i].id;
 
           if (TerraMA2WebComponents.MapDisplay.addImageWMSLayer(layerId, layerName, layerName, uriGeoServer + '/ows', serverType, false, false, "terrama2-layerexplorer", {version: "1.1.0"})){
-            TerraMA2WebComponents.LayerExplorer.addLayersFromMap(layerId, data[i].type, null, "treeview unsortable terrama2-truncate-text", null);
-            TerraMA2WebComponents.MapDisplay.setLayerProperty(layerId, "layerType", data[i].type);
+            TerraMA2WebComponents.LayerExplorer.addLayersFromMap(layerId, parent, null, "treeview unsortable terrama2-truncate-text", null);
+            TerraMA2WebComponents.MapDisplay.setLayerProperty(layerId, "layerType", parent);
             TerraMA2WebComponents.MapDisplay.setLayerProperty(layerId, "layerName", layerName);
-            var objectLayer = createLayerObject(data[i]);
-            addLayer(objectLayer);
-            if (data[i].type == 'analysis' || data[i].type == 'dynamic'){
-              var url = uriGeoServer + '/' + workspace + '/' + data[i].layers[0] + '/wms?service=WMS&version=1.1.0&request=GetCapabilities';
+            //var objectLayer = createLayerObject(data[i]);
+            //addLayer(objectLayer);
+            if (parent == 'analysis' || parent == 'dynamic'){
+              var url = uriGeoServer + '/' + workspace + '/' + data[i].nameId + '/wms?service=WMS&version=1.1.0&request=GetCapabilities';
               var getCapabilitiesUrl = {
-                layerName: data[i].layers[0],
+                layerName: data[i].nameId,
                 layerId: layerId,
-                parent: data[i].type,
+                parent: parent,
                 url: url,
                 format: 'xml',
                 update: false
@@ -82,10 +98,11 @@ define(
               Utils.getSocket().emit('proxyRequestCapabilities', getCapabilitiesUrl);
             }
           }
-          LayerStatus.changeGroupStatusIcon(data[i].type, "working");
+          LayerStatus.changeGroupStatusIcon(parent, "working");
           LayerStatus.addLayerStatusIcon(layerId);
           LayerStatus.changeLayerStatusIcon(layerId, "working");
           Sortable.addLayerToSort(layerId, layerName);
+          //Utils.getSocket().emit('checkConnection', {url: uriGeoServer, requestId: layerId});
 				}
 			}
     };
@@ -96,7 +113,8 @@ define(
       addLayersToSort: addLayersToSort,
       createLayerObject: createLayerObject,
       addLayer: addLayer,
-      getAllLayers: getAllLayers
+      getAllLayers: getAllLayers,
+      getLayerById: getLayerById
     }
     
   }
