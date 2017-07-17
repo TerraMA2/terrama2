76553090021966631cade2ba6939cd0a0dd99dad
Jean Souza <jeancfsouza@gmail.com>
2017-07-17 16:37:29 -0300

    improving exportation

--- webmonitor/public/javascripts/components/LayerToolbox.js
+++ webmonitor/public/javascripts/components/LayerToolbox.js
@@ -10,10 +10,7 @@ define(
         } else if(result.progress !== undefined) {
           console.log(result.progress + '% Completo');
         } else {
-          console.log("folder=" + result.folder + "&file=" + result.file);
-
-          var exportLink = "http://localhost:36000/export?folder=" + result.folder + "&file=" + result.file;
-
+          var exportLink = webadminHostInfo.protocol + webadminHostInfo.host + ":" + webadminHostInfo.port + webadminHostInfo.basePath + "export?folder=" + result.folder + "&file=" + result.file;
           $('#exportation-iframe').attr('src', exportLink);
         }
       });
@@ -23,16 +20,22 @@ define(
       $('#export').on('click', function() {
         var layer = Layers.getLayerById($(this).data("layerid"));
 
-        if(layer.exportation !== null) {
-          Utils.getWebAppSocket().emit('generateFileRequest', {
+        if(layer !== null && layer.exportation !== null) {
+          var exportationParams = {
             format: $("#exportation-type").val().toString(),
-            dateTimeFrom: '2017-07-10 00:00:00',
-            dateTimeTo: '2017-07-12 23:59:59',
             schema: layer.exportation.schema,
             table: layer.exportation.table,
-            dateTimeField: layer.exportation.dateField,
-            dataProviderId: layer.exportation.dataProviderId
-          });
+            dataProviderId: layer.exportation.dataProviderId,
+            fileName: layer.name
+          };
+
+          if(layer.exportation.dateField !== null) {
+            exportationParams.dateTimeField = layer.exportation.dateField;
+            exportationParams.dateTimeFrom = $("#terrama2-calendar").find("input[type='hidden']").attr("start-date");
+            exportationParams.dateTimeTo = $("#terrama2-calendar").find("input[type='hidden']").attr("end-date");
+          }
+
+          Utils.getWebAppSocket().emit('generateFileRequest', exportationParams);
         }
 			});
 
@@ -40,13 +43,25 @@ define(
         var layer = Layers.getLayerById($(this).parent().data("layerid"));
 
         if(layer !== null) {
-          $("#layer-toolbox > .layer-toolbox-body > #slider-box").empty().html("<label></label><br/><div id=\"opacity" + layer.htmlId + "\"></div>");
-          $("#export").data("layerid", layer.id);
-          
-          var currentOpacity = TerraMA2WebComponents.MapDisplay.getLayerOpacity(layer.id) * 100;
-          Slider.setOpacitySlider(layer.id, currentOpacity);
+          if(layer.exportation !== null) {
+            $("#export").data("layerid", layer.id);
+
+            if($("#exportation-box").hasClass("hidden"))
+              $("#exportation-box").removeClass("hidden");
+
+            $("#layer-toolbox").css("height", "265px");
+          } else {
+            if(!$("#exportation-box").hasClass("hidden"))
+              $("#exportation-box").addClass("hidden");
+
+            $("#layer-toolbox").css("height", "100px");
+          }
 
           $("#layer-toolbox > .layer-toolbox-header .layer-name").text(layer.name);
+
+          $("#layer-toolbox > .layer-toolbox-body > #slider-box").empty().html("<label></label><br/><div id=\"opacity" + layer.htmlId + "\"></div>");
+          var currentOpacity = TerraMA2WebComponents.MapDisplay.getLayerOpacity(layer.id) * 100;
+          Slider.setOpacitySlider(layer.id, currentOpacity);
 
           if($("#layer-toolbox").hasClass("hidden"))
             $("#layer-toolbox").removeClass("hidden");
