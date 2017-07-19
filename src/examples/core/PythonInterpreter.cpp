4598017a69e86683ea1c72e0de6d5ab192679667
janosimas <janosimas@gmail.com>
2017-07-19 16:43:15 -0300

    better example

--- src/examples/core/PythonInterpreter.cpp
+++ src/examples/core/PythonInterpreter.cpp
@@ -10,9 +10,33 @@ int main(int argc, char* argv[])
 
   {
     terrama2::core::PythonInterpreter interpreter;
+    terrama2::core::PythonInterpreter interpreter2;
     interpreter.setDouble("teste", 10);
-    interpreter.runScript("teste = 20");
-    std::cout << "teste: " << *interpreter.getNumeric("teste") << std::endl;
+    std::cout << "== c++\n" << "teste: " << *interpreter.getNumeric("teste") << std::endl;
+    interpreter.runScript("print('== python')\n"
+                          "print(teste)\n"
+                          "teste = 20");
+
+    interpreter2.runScript("print('== python2')\n"
+                           "print(teste)");
+
+    std::cout << "== c++\n" << "teste: " << *interpreter.getNumeric("teste") << std::endl;
+    interpreter.runScript("print('== python')\n"
+                          "print(teste)");
+  }
+
+  {
+    std::cout << "============================" << std::endl;
+    std::cout << "New python state" << std::endl;
+    terrama2::core::PythonInterpreter interpreter;
+    interpreter.setDouble("teste", 10);
+    std::cout << "c++\n" << "teste: " << *interpreter.getNumeric("teste") << std::endl;
+    interpreter.runScript("print('python')\n"
+                          "print(teste)\n"
+                          "teste = 20");
+    std::cout << "c++\n" << "teste: " << *interpreter.getNumeric("teste") << std::endl;
+    interpreter.runScript("print('python')\n"
+                          "print(teste)");
   }
 
   PyEval_AcquireLock();
