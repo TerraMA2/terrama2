a64cd1b343b8ab43d643177765ae5a4f9fa918fe
Jean Souza <jeancfsouza@gmail.com>
2017-08-24 09:38:25 -0300

    small fixes and improvements

--- webapp/public/javascripts/angular/status/status.js
+++ webapp/public/javascripts/angular/status/status.js
@@ -232,9 +232,10 @@ define([
             var obj = currentProcess[targetKey] || {name: currentProcess.name};
 
             var projectName = "";
+            var projectId = (currentProcess.class === "Collector" ? currentProcess.dataSeriesOutput.project_id : currentProcess.project_id);
 
             for(var i = 0, projectsLength = config.projects.length; i < projectsLength; i++) {
-              if(currentProcess.project_id === config.projects[i].id) {
+              if(projectId === config.projects[i].id) {
                 projectName = config.projects[i].name;
                 break;
               }
