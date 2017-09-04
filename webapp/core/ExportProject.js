731e255f3c83fdeccdce86b5919118891122ec25
franciscovn <franciscovn909@gmail.com>
2017-09-04 10:28:55 -0300

    Create separated file to import a project

---
+++ webapp/core/ExportProject.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * Class responsible to export a project
+ * @class ExportProject
+ * 
+ * 
+ * @property {object} json - Object with data to export.
+ * @property {object} callback - Function to callback the result.
+ */
+var ExportProject = function(json, callback){
+
+}
+
+module.exports = ExportProject;
