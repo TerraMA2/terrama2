b88dd05a09d4542a814d809d948983c5590d163f
franciscovn <franciscovn909@gmail.com>
2017-09-05 09:56:15 -0300

    Verifying map server and email server before check connection

--- webapp/sockets/ServiceConnectionsChecker.js
+++ webapp/sockets/ServiceConnectionsChecker.js
@@ -52,33 +52,51 @@ var ServiceConnectionsChecker = function(io) {
           memberServicePortNumberChecker(paramsToCheckPort, function(response){
             responseObject.portNumberConnectionResponse = response;
             if (serviceObject.service_type_id == ServiceTypeEnum.ALERT){
-              var emailServerUrl = url.parse(serviceObject.metadata.email_server);
-              var username = emailServerUrl.auth.split(":")[0];
-              var password = emailServerUrl.auth.split(":")[1];
-              var paramsToCheckEmailServer = {
-                host: emailServerUrl.hostname,
-                port: emailServerUrl.port,
-                username: username,
-                password: password
-              };
-              memberSMTPConnectionChecker(paramsToCheckEmailServer, function(response){
-                responseObject.smtpConnectionResponse = response;
+              if (serviceObject.metadata.email_server){
+                var emailServerUrl = url.parse(serviceObject.metadata.email_server);
+                var username = emailServerUrl.auth.split(":")[0];
+                var password = emailServerUrl.auth.split(":")[1];
+                var paramsToCheckEmailServer = {
+                  host: emailServerUrl.hostname,
+                  port: emailServerUrl.port,
+                  username: username,
+                  password: password
+                };
+                memberSMTPConnectionChecker(paramsToCheckEmailServer, function(response){
+                  responseObject.smtpConnectionResponse = response;
+                  client.emit('testServiceConnectionsResponse', responseObject);
+                });
+              } else {
+                var smtpConnectionResponse = {
+                  error: true,
+                  message: 'Invalid email server URL'
+                };
+                responseObject.smtpConnectionResponse = smtpConnectionResponse;
                 client.emit('testServiceConnectionsResponse', responseObject);
-              });
+              }
             } else if (serviceObject.service_type_id == ServiceTypeEnum.VIEW){
-              var mapServerUrl = url.parse(serviceObject.metadata.maps_server);
-              var username = mapServerUrl.auth.split(":")[0];
-              var password = mapServerUrl.auth.split(":")[1];
-              var paramsToCheckMapServer = {
-                host: mapServerUrl.hostname + mapServerUrl.path,
-                port: mapServerUrl.port,
-                username: username,
-                password: password
-              };
-              memberGeoServerConnection(paramsToCheckMapServer, function(response){
-                responseObject.mapServerConnectionResponse = response;
+              if (serviceObject.metadata.maps_server){
+                var mapServerUrl = url.parse(serviceObject.metadata.maps_server);
+                var username = mapServerUrl.auth.split(":")[0];
+                var password = mapServerUrl.auth.split(":")[1];
+                var paramsToCheckMapServer = {
+                  host: mapServerUrl.hostname + mapServerUrl.path,
+                  port: mapServerUrl.port,
+                  username: username,
+                  password: password
+                };
+                memberGeoServerConnection(paramsToCheckMapServer, function(response){
+                  responseObject.mapServerConnectionResponse = response;
+                  client.emit('testServiceConnectionsResponse', responseObject);
+                });
+              } else {
+                var mapServerConnectionResponse = {
+                  error: true,
+                  message: 'Invalid map server URL'
+                };
+                responseObject.mapServerConnectionResponse = mapServerConnectionResponse;
                 client.emit('testServiceConnectionsResponse', responseObject);
-              });
+              }
             } else {
               client.emit('testServiceConnectionsResponse', responseObject);
             }
