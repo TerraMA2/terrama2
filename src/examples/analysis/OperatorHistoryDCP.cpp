01286823252277359d62ce48e9bfe6cf49a11865
BiancaMaciel <bianca_maciel.c@hotmail.com>
2017-09-06 13:47:48 -0300

    Modification in code OperatorHistoryDCP.cpp has been inserted google mock

--- src/examples/analysis/OperatorHistoryDCP.cpp
+++ src/examples/analysis/OperatorHistoryDCP.cpp
@@ -9,13 +9,16 @@
 #include <terrama2/core/data-model/DataSet.hpp>
 #include <terrama2/core/data-model/DataSetDcp.hpp>
 
-#include <terrama2/services/analysis/core/Shared.hpp>
-#include <terrama2/services/analysis/core/Analysis.hpp>
+#include <terrama2/services/analysis/core/python/PythonInterpreter.hpp>
 #include <terrama2/services/analysis/core/utility/PythonInterpreterInit.hpp>
+#include <terrama2/services/analysis/core/Analysis.hpp>
 #include <terrama2/services/analysis/core/Service.hpp>
 #include <terrama2/services/analysis/core/DataManager.hpp>
 
+#include <terrama2/services/analysis/mock/MockAnalysisLogger.hpp>
+
 #include <terrama2/impl/Utils.hpp>
+#include <terrama2/Config.hpp>
 
 #include <iostream>
 
@@ -23,6 +26,7 @@
 #include <QTimer>
 #include <QCoreApplication>
 #include <QUrl>
+
 
 using namespace terrama2::services::analysis::core;
 
@@ -32,21 +36,49 @@ int main(int argc, char* argv[])
 
   terrama2::core::registerFactories();
 
-
-  auto& serviceManager = terrama2::core::ServiceManager::getInstance();
-te::core::URI uri("pgsql://"+TERRAMA2_DATABASE_USERNAME+":"+TERRAMA2_DATABASE_PASSWORD+"@"+TERRAMA2_DATABASE_HOST+":"+TERRAMA2_DATABASE_PORT+"/"+TERRAMA2_DATABASE_DBNAME);
-  serviceManager.setLogConnectionInfo(uri);
-
   terrama2::services::analysis::core::PythonInterpreterInit pythonInterpreterInit;
 
   QCoreApplication app(argc, argv);
 
+  auto& serviceManager = terrama2::core::ServiceManager::getInstance();
 
-  DataManagerPtr dataManager(new DataManager());
+  auto dataManager = std::make_shared<terrama2::services::analysis::core::DataManager>();
+
+  auto loggerCopy = std::make_shared<terrama2::core::MockAnalysisLogger>();
+
+  EXPECT_CALL(*loggerCopy, setConnectionInfo(::testing::_)).WillRepeatedly(::testing::Return());
+  EXPECT_CALL(*loggerCopy, setTableName(::testing::_)).WillRepeatedly(::testing::Return());
+  EXPECT_CALL(*loggerCopy, getLastProcessTimestamp(::testing::_)).WillRepeatedly(::testing::Return(nullptr));
+  EXPECT_CALL(*loggerCopy, getDataLastTimestamp(::testing::_)).WillRepeatedly(::testing::Return(nullptr));
+  EXPECT_CALL(*loggerCopy, done(::testing::_, ::testing::_)).WillRepeatedly(::testing::Return());
+  EXPECT_CALL(*loggerCopy, start(::testing::_)).WillRepeatedly(::testing::Return(0));
+  EXPECT_CALL(*loggerCopy, isValid()).WillRepeatedly(::testing::Return(true));
+
+  auto logger = std::make_shared<terrama2::core::MockAnalysisLogger>();
+
+  EXPECT_CALL(*logger, setConnectionInfo(::testing::_)).WillRepeatedly(::testing::Return());
+  EXPECT_CALL(*logger, setTableName(::testing::_)).WillRepeatedly(::testing::Return());
+  EXPECT_CALL(*logger, getLastProcessTimestamp(::testing::_)).WillRepeatedly(::testing::Return(nullptr));
+  EXPECT_CALL(*logger, getDataLastTimestamp(::testing::_)).WillRepeatedly(::testing::Return(nullptr));
+  EXPECT_CALL(*logger, done(::testing::_, ::testing::_)).WillRepeatedly(::testing::Return());
+  EXPECT_CALL(*logger, start(::testing::_)).WillRepeatedly(::testing::Return(0));
+  EXPECT_CALL(*logger, clone()).WillRepeatedly(::testing::Return(loggerCopy));
+  EXPECT_CALL(*logger, isValid()).WillRepeatedly(::testing::Return(true));
+
+  te::core::URI uri("pgsql://"+TERRAMA2_DATABASE_USERNAME+":"+TERRAMA2_DATABASE_PASSWORD+"@"+TERRAMA2_DATABASE_HOST+":"+TERRAMA2_DATABASE_PORT+"/"+TERRAMA2_DATABASE_DBNAME);
+
+  Service service(dataManager);
+  serviceManager.setInstanceId(1);
+  serviceManager.setLogger(logger);
+  serviceManager.setLogConnectionInfo(te::core::URI(""));
+  serviceManager.setInstanceId(1);
+
+  service.setLogger(logger);
+  service.start();
+
 
   // DataProvider information
-  terrama2::core::DataProvider* outputDataProvider = new terrama2::core::DataProvider();
-  terrama2::core::DataProviderPtr outputDataProviderPtr(outputDataProvider);
+  std::shared_ptr<terrama2::core::DataProvider> outputDataProvider = std::make_shared<terrama2::core::DataProvider>();
   outputDataProvider->id = 3;
   outputDataProvider->name = "DataProvider postgis";
   outputDataProvider->uri = uri.uri();
@@ -55,18 +87,17 @@ te::core::URI uri("pgsql://"+TERRAMA2_DATABASE_USERNAME+":"+TERRAMA2_DATABASE_PA
   outputDataProvider->active = true;
 
 
-  dataManager->add(outputDataProviderPtr);
+  dataManager->add(outputDataProvider);
 
 
   auto& semanticsManager = terrama2::core::SemanticsManager::getInstance();
 
   // DataSeries information
-  terrama2::core::DataSeries* outputDataSeries = new terrama2::core::DataSeries();
-  terrama2::core::DataSeriesPtr outputDataSeriesPtr(outputDataSeries);
+  std::shared_ptr<terrama2::core::DataSeries> outputDataSeries = std::make_shared<terrama2::core::DataSeries>();
   outputDataSeries->id = 3;
   outputDataSeries->name = "Analysis result";
   outputDataSeries->semantics = semanticsManager.getSemantics("ANALYSIS_MONITORED_OBJECT-postgis");
-  outputDataSeries->dataProviderId = outputDataProviderPtr->id;
+  outputDataSeries->dataProviderId = outputDataProvider->id;
 
 
   // DataSet information
@@ -79,7 +110,7 @@ te::core::URI uri("pgsql://"+TERRAMA2_DATABASE_USERNAME+":"+TERRAMA2_DATABASE_PA
   outputDataSeries->datasetList.emplace_back(outputDataSet);
 
 
-  dataManager->add(outputDataSeriesPtr);
+  dataManager->add(outputDataSeries);
 
   std::string script = "moBuffer = Buffer(BufferType.Out_union, 2., \"km\")\n"
           "ids = dcp.zonal.influence.by_rule(\"Serra do Mar\", moBuffer)\n"
@@ -99,15 +130,14 @@ te::core::URI uri("pgsql://"+TERRAMA2_DATABASE_USERNAME+":"+TERRAMA2_DATABASE_PA
           "add_value(\"history_standard_deviation\",x)\n";
 
 
-  Analysis* analysis = new Analysis;
-  AnalysisPtr analysisPtr(analysis);
-
+  std::shared_ptr<terrama2::services::analysis::core::Analysis> analysis = std::make_shared<terrama2::services::analysis::core::Analysis>();
   analysis->id = 1;
   analysis->name = "History DCP";
   analysis->script = script;
   analysis->scriptLanguage = ScriptLanguage::PYTHON;
   analysis->type = AnalysisType::MONITORED_OBJECT_TYPE;
   analysis->outputDataSeriesId = 3;
+  analysis->outputDataSetId = outputDataSet->id;
   analysis->active = true;
   analysis->serviceInstanceId = 1;
 
@@ -116,41 +146,36 @@ te::core::URI uri("pgsql://"+TERRAMA2_DATABASE_USERNAME+":"+TERRAMA2_DATABASE_PA
   analysis->metadata["INFLUENCE_RADIUS"] = "50";
   analysis->metadata["INFLUENCE_RADIUS_UNIT"] = "km";
 
-  terrama2::core::DataProvider* dataProvider = new terrama2::core::DataProvider();
-  terrama2::core::DataProviderPtr dataProviderPtr(dataProvider);
+  std::shared_ptr<terrama2::core::DataProvider> dataProvider = std::make_shared<terrama2::core::DataProvider>();
   dataProvider->name = "Provider";
-  dataProvider->uri += TERRAMA2_DATA_DIR;
-  dataProvider->uri += "/shapefile";
+  dataProvider->uri = uri.uri();
   dataProvider->intent = terrama2::core::DataProviderIntent::COLLECTOR_INTENT;
-  dataProvider->dataProviderType = "FILE";
+  dataProvider->dataProviderType = "POSTGIS";
   dataProvider->active = true;
   dataProvider->id = 1;
 
 
-  dataManager->add(dataProviderPtr);
+  dataManager->add(dataProvider);
 
-  terrama2::core::DataSeries* dataSeries = new terrama2::core::DataSeries();
-  terrama2::core::DataSeriesPtr dataSeriesPtr(dataSeries);
+  std::shared_ptr<terrama2::core::DataSeries> dataSeries = std::make_shared<terrama2::core::DataSeries>();
   dataSeries->dataProviderId = dataProvider->id;
-  dataSeries->semantics = semanticsManager.getSemantics("STATIC_DATA-ogr");
+  dataSeries->semantics = semanticsManager.getSemantics("STATIC_DATA-postgis");
   dataSeries->name = "Monitored Object";
   dataSeries->id = 1;
   dataSeries->dataProviderId = 1;
+  dataSeries->active = true;
 
   //DataSet information
-  terrama2::core::DataSet* dataSet = new terrama2::core::DataSet;
-  terrama2::core::DataSetPtr dataSetPtr(dataSet);
+  std::shared_ptr<terrama2::core::DataSet> dataSet = std::make_shared<terrama2::core::DataSet>();
   dataSet->active = true;
-  dataSet->format.emplace("mask", "estados_2010.shp");
-  dataSet->format.emplace("srid", "4326");
+  dataSet->format.emplace("table_name", "estados_2010");
   dataSet->id = 1;
 
-  dataSeries->datasetList.push_back(dataSetPtr);
-  dataManager->add(dataSeriesPtr);
+  dataSeries->datasetList.push_back(dataSet);
+  dataManager->add(dataSeries);
 
 
-  terrama2::core::DataProvider* dataProvider2 = new terrama2::core::DataProvider();
-  terrama2::core::DataProviderPtr dataProvider2Ptr(dataProvider2);
+  std::shared_ptr<terrama2::core::DataProvider> dataProvider2 = std::make_shared<terrama2::core::DataProvider>();
   dataProvider2->name = "Provider";
   dataProvider2->uri += TERRAMA2_DATA_DIR;
   dataProvider2->uri += "/PCD_serrmar_INPE";
@@ -160,53 +185,52 @@ te::core::URI uri("pgsql://"+TERRAMA2_DATABASE_USERNAME+":"+TERRAMA2_DATABASE_PA
   dataProvider2->id = 2;
 
 
-  dataManager->add(dataProvider2Ptr);
+  dataManager->add(dataProvider2);
 
   AnalysisDataSeries monitoredObjectADS;
   monitoredObjectADS.id = 1;
-  monitoredObjectADS.dataSeriesId = dataSeriesPtr->id;
+  monitoredObjectADS.dataSeriesId = dataSeries->id;
   monitoredObjectADS.type = AnalysisDataSeriesType::DATASERIES_MONITORED_OBJECT_TYPE;
   monitoredObjectADS.metadata["identifier"] = "nome";
 
 
   //DataSeries information
-  terrama2::core::DataSeries* dcpSeries = new terrama2::core::DataSeries;
-  terrama2::core::DataSeriesPtr dcpSeriesPtr(dcpSeries);
+  std::shared_ptr<terrama2::core::DataSeries> dcpSeries = std::make_shared<terrama2::core::DataSeries>();
   dcpSeries->dataProviderId = dataProvider2->id;
-
   dcpSeries->semantics = semanticsManager.getSemantics("DCP-inpe");
   dcpSeries->name = "Serra do Mar";
   dcpSeries->id = 2;
   dcpSeries->dataProviderId = 2;
+  dcpSeries->active = true;
 
   //DataSet information
-  terrama2::core::DataSetDcp* dcpDataset69034 = new terrama2::core::DataSetDcp;
-  terrama2::core::DataSetDcpPtr dcpDataset69034Ptr(dcpDataset69034);
+  std::shared_ptr<terrama2::core::DataSetDcp> dcpDataset69034 = std::make_shared<terrama2::core::DataSetDcp>();
   dcpDataset69034->active = true;
-  dcpDataset69034->format.emplace("mask", "69033.txt");
+  dcpDataset69034->format.emplace("mask", "69034.txt");
+  dcpDataset69034->format.emplace("alias", "dcp_69034");
   dcpDataset69034->format.emplace("timezone", "-02:00");
   dcpDataset69034->dataSeriesId = 2;
   dcpDataset69034->id = 2;
   dcpDataset69034->position = std::shared_ptr<te::gm::Point>(new te::gm::Point(-44.46540, -23.00506, 4618, te::gm::PointType, nullptr));
-  dcpSeries->datasetList.push_back(dcpDataset69034Ptr);
+  dcpSeries->datasetList.push_back(dcpDataset69034);
 
 
-  terrama2::core::DataSetDcp* dcpDataset30885 = new terrama2::core::DataSetDcp;
-  terrama2::core::DataSetDcpPtr dcpDataset30886Ptr(dcpDataset30885);
+  std::shared_ptr<terrama2::core::DataSetDcp> dcpDataset30885 = std::make_shared<terrama2::core::DataSetDcp>();
   dcpDataset30885->active = true;
   dcpDataset30885->format.emplace("mask", "30885.txt");
+  dcpDataset30885->format.emplace("alias", "dcp_30885");
   dcpDataset30885->format.emplace("timezone", "-02:00");
   dcpDataset30885->dataSeriesId = 2;
   dcpDataset30885->id = 3;
   dcpDataset30885->position = std::shared_ptr<te::gm::Point>(new te::gm::Point(-46.121, -23.758, 4618, te::gm::PointType, nullptr));
-  dcpSeries->datasetList.push_back(dcpDataset30886Ptr);
+  dcpSeries->datasetList.push_back(dcpDataset30885);
 
   AnalysisDataSeries dcpADS;
   dcpADS.id = 2;
-  dcpADS.dataSeriesId = dcpSeriesPtr->id;
+  dcpADS.dataSeriesId = dcpSeries->id;
   dcpADS.type = AnalysisDataSeriesType::ADDITIONAL_DATA_TYPE;
 
-  dataManager->add(dcpSeriesPtr);
+  dataManager->add(dcpSeries);
 
 
   std::vector<AnalysisDataSeries> analysisDataSeriesList;
@@ -214,26 +238,14 @@ te::core::URI uri("pgsql://"+TERRAMA2_DATABASE_USERNAME+":"+TERRAMA2_DATABASE_PA
   analysisDataSeriesList.push_back(monitoredObjectADS);
   analysis->analysisDataSeriesList = analysisDataSeriesList;
 
-  dataManager->add(analysisPtr);
+  dataManager->add(analysis);
 
-  // Starts the service and adds the analysis
-  Service service(dataManager);
-  terrama2::core::ServiceManager::getInstance().setInstanceId(1);
-
-  auto logger = std::make_shared<AnalysisLogger>();
-  logger->setConnectionInfo(uri);
-  service.setLogger(logger);
-
-  service.start();
-  service.addToQueue(analysisPtr->id, terrama2::core::TimeUtils::nowUTC());
+  service.addToQueue(analysis->id, terrama2::core::TimeUtils::nowUTC());
 
   QTimer timer;
   QObject::connect(&timer, SIGNAL(timeout()), QCoreApplication::instance(), SLOT(quit()));
   timer.start(100000);
   app.exec();
-
-
-
 
   return 0;
 }
