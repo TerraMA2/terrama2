318dcdf30dc2502583229d53ae89259d93008b95
Jean Souza <jeancfsouza@gmail.com>
2017-09-06 12:21:25 -0300

    fixing python initialization

--- src/terrama2/core/interpreter/PythonInterpreter.cpp
+++ src/terrama2/core/interpreter/PythonInterpreter.cpp
@@ -59,13 +59,6 @@ terrama2::core::PythonInterpreter::PythonInterpreter()
   : Interpreter(),
   impl_(new Impl())
 {
-  if(Py_IsInitialized())
-  {
-    Py_Finalize();
-  }
-
-  Py_Initialize();
-
   impl_->mainThreadState_ = PyThreadState_Get();
   StateLock lock(impl_->mainThreadState_);
   impl_->interpreterState_ = Py_NewInterpreter();
