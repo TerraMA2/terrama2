0b59e98363fd3d437367f277a0e8eba997c9c1d9
franciscovn <franciscovn909@gmail.com>
2017-09-12 09:18:27 -0300

    Updating project user_id

--- webapp/core/DataManager.js
+++ webapp/core/DataManager.js
@@ -577,9 +577,14 @@ var DataManager = module.exports = {
     var self = this;
     return new Promise(function(resolve, reject) {
       self.getProject({id: projectObject.id}).then(function(project) {
+        var fieldsToUpdate = ["name", "description", "version", "protected"];
+        // Add user if the project dont have one
+        if (!project.user_id){
+          fieldsToUpdate.push("user_id");
+        }
 
         return models.db.Project.update(projectObject, Utils.extend({
-          fields: ["name", "description", "version", "protected"],
+          fields: fieldsToUpdate,
           where: {
             id: project.id
           }
@@ -589,6 +594,7 @@ var DataManager = module.exports = {
           projectItem.description = projectObject.description;
           projectItem.version = projectObject.version;
           projectItem.protected = projectObject.protected;
+          projectItem.user_id = projectObject.user_id;
 
           return resolve(Utils.clone(projectItem));
         }).catch(function(err) {
