6d471163d0367e0c44b36239b4ebcebda34b21b1
franciscovn <franciscovn909@gmail.com>
2017-09-19 08:29:48 -0300

    [Partial] Interpolation data-model

--- webapp/core/data-model/Interpolator.js
+++ webapp/core/data-model/Interpolator.js
@@ -8,6 +8,7 @@
     // Calling base abstraction
     AbstractClass.call(this, {'class': 'Interpolator'});
     this.id = params.id;
+    this.active = params.active;
     this.project_id = params.project_id;
     this.service_instance_id = params.service_instance_id;
     this.data_series_input = params.data_series_input;
@@ -23,7 +24,41 @@
     this.automaticSchedule = params.automaticSchedule || {};
     this.dataSeriesOutput = params.dataSeriesOutput || {};
     this.dataSeriesInput = params.dataSeriesInput || {};
+    if (params.InterpolatorMetadata){
+      this.setMetadata(params.InterpolatorMetadata);
+    } else {
+      this.metadata = params.interpolator_metadata;
+    }
   }
 
+  Interpolator.prototype.setMetadata = function(formats) {
+    var formatOutput = {};
+    if (formats instanceof Array) {
+      formats.forEach(function(format) {
+        formatOutput[format.key] = format.value;
+      });
+    } else {
+      formatOutput = formats;
+    }
+    this.metadata = formatOutput;
+  };
+
+  Interpolator.prototype.toObject = function() {
+    return Object.assign(AbstractClass.prototype.toObject.call(this), {
+      id: this.id,
+      active: this.active,
+      project_id: params.project_id,
+      service_instance_id: params.service_instance_id,
+      data_series_input: params.data_series_input,
+      data_series_output: params.data_series_output,
+      schedule_type: params.schedule_type,
+      bounding_rect: params.bounding_rect,
+      interpolation_attribute: params.interpolation_attribute,
+      resolution_x: params.resolution_x,
+      resolution_y: params.resolution_y,
+      srid: params.srid,
+      metadata: this.metadata
+    });
+  };
   module.exports = Interpolator;
 }());
