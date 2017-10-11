26b3990003f7087df970004fcd5e11aa035a226b
BiancaMaciel <bianca_maciel.c@hotmail.com>
2017-10-11 14:56:57 -0300

    Implemented json in file CollectorAndCropRaster

--- src/examples/collector/CollectorAndCropRaster.cpp
+++ src/examples/collector/CollectorAndCropRaster.cpp
@@ -49,9 +49,14 @@
 #include <terrama2/services/collector/core/Collector.hpp>
 #include <terrama2/services/analysis/core/Service.hpp>
 
+#include <terrama2/services/collector/mock/MockCollectorLogger.hpp>
+
 #include <terrama2/impl/Utils.hpp>
 
 #include <terrama2/core/data-access/DataRetriever.hpp>
+
+#include <examples/data/GradsFTP.hpp>
+
 
 // STL
 #include <memory>
@@ -62,130 +67,84 @@
 #include <QTimer>
 #include <QUrl>
 
-void addInput(std::shared_ptr<terrama2::services::collector::core::DataManager> dataManager)
-{
-  ///////////////////////////////////////////////
-  //     input
-  // DataProvider information
-  terrama2::core::DataProvider* dataProvider = new terrama2::core::DataProvider();
-  terrama2::core::DataProviderPtr dataProviderPtr(dataProvider);
-  dataProvider->uri = "ftp://terrama:curso@server-ftpdsa.cptec.inpe.br";
-
-  dataProvider->intent = terrama2::core::DataProviderIntent::COLLECTOR_INTENT;
-  dataProvider->active = true;
-  dataProvider->id = 1;
-  dataProvider->name = "Dados DSA curso";
-  dataProvider->dataProviderType = "FTP";
-
-  dataManager->add(dataProviderPtr);
-
-  auto& semanticsManager = terrama2::core::SemanticsManager::getInstance();
-  // DataSeries information
-  terrama2::core::DataSeries* dataSeries = new terrama2::core::DataSeries();
-  terrama2::core::DataSeriesPtr dataSeriesPtr(dataSeries);
-  dataSeries->id = 1;
-  dataSeries->name = "Hidroestimador_input";
-  dataSeries->semantics = semanticsManager.getSemantics("GRID-grads");
-  dataSeries->dataProviderId = dataProviderPtr->id;
-
-  terrama2::core::DataSetOccurrence* dataSet = new terrama2::core::DataSetOccurrence();
-  dataSet->id = 1;
-  dataSet->active = true;
-  dataSet->format.emplace("ctl_filename", "racc.ctl");
-  dataSet->format.emplace("folder", "hidro");
-  dataSet->format.emplace("srid", "4326");
-  dataSet->format.emplace("timezone", "UTC+00");
-  dataSet->format.emplace("data_type", "INT16");
-  dataSet->format.emplace("number_of_bands", "1");
-  dataSet->format.emplace("bytes_before", "0");
-  dataSet->format.emplace("bytes_after", "0");
-
-  dataSeries->datasetList.emplace_back(dataSet);
-
-  dataManager->add(dataSeriesPtr);
-}
-
-void addOutput(std::shared_ptr<terrama2::services::collector::core::DataManager> dataManager)
-{
-  ///////////////////////////////////////////////
-  //     output
-
-  // DataProvider information
-  terrama2::core::DataProvider* outputDataProvider = new terrama2::core::DataProvider();
-  terrama2::core::DataProviderPtr outputDataProviderPtr(outputDataProvider);
-  outputDataProvider->id = 2;
-  outputDataProvider->name = "Dados locais";
-  outputDataProvider->uri = "file://"+TERRAMA2_DATA_DIR;
-  outputDataProvider->intent = terrama2::core::DataProviderIntent::COLLECTOR_INTENT;
-  outputDataProvider->dataProviderType = "FILE";
-  outputDataProvider->active = true;
-
-  dataManager->add(outputDataProviderPtr);
-
-  // DataSeries information
-  terrama2::core::DataSeries* outputDataSeries = new terrama2::core::DataSeries();
-  terrama2::core::DataSeriesPtr outputDataSeriesPtr(outputDataSeries);
-  outputDataSeries->id = 2;
-  outputDataSeries->name = "Hidroestimador";
-  auto& semanticsManager = terrama2::core::SemanticsManager::getInstance();
-  outputDataSeries->semantics = semanticsManager.getSemantics("GRID-gdal");
-  outputDataSeries->dataProviderId = outputDataProviderPtr->id;
-
-  dataManager->add(outputDataSeriesPtr);
-
-  // DataSet information
-  terrama2::core::DataSetOccurrence* outputDataSet = new terrama2::core::DataSetOccurrence();
-  outputDataSet->active = true;
-  outputDataSet->id = 2;
-  outputDataSet->dataSeriesId = outputDataSeries->id;
-  outputDataSet->format.emplace("mask", "S10238225_%YYYY%MM%DD%hh%mm");
-  outputDataSet->format.emplace("folder", "hidroestimador_crop");
-
-  outputDataSeries->datasetList.emplace_back(outputDataSet);
-}
 
 int main(int argc, char* argv[])
 {
   try
   {
     terrama2::core::TerraMA2Init terramaRaii("example", 0);
-    terrama2::core::registerFactories();
+    Q_UNUSED(terramaRaii);
 
+    terrama2::core::registerFactories();
     {
       QCoreApplication app(argc, argv);
+
       auto& serviceManager = terrama2::core::ServiceManager::getInstance();
-      te::core::URI uri("pgsql://"+TERRAMA2_DATABASE_USERNAME+":"+TERRAMA2_DATABASE_PASSWORD+"@"+TERRAMA2_DATABASE_HOST+":"+TERRAMA2_DATABASE_PORT+"/"+TERRAMA2_DATABASE_DBNAME);
-      serviceManager.setLogConnectionInfo(uri);
-      serviceManager.setInstanceId(1);
 
       auto dataManager = std::make_shared<terrama2::services::collector::core::DataManager>();
 
-      addInput(dataManager);
-      addOutput(dataManager);
+      auto loggerCopy = std::make_shared<terrama2::core::MockCollectorLogger>();
+
+      EXPECT_CALL(*loggerCopy, setConnectionInfo(::testing::_)).WillRepeatedly(::testing::Return());
+      EXPECT_CALL(*loggerCopy, setTableName(::testing::_)).WillRepeatedly(::testing::Return());
+      EXPECT_CALL(*loggerCopy, getLastProcessTimestamp(::testing::_)).WillRepeatedly(::testing::Return(nullptr));
+      EXPECT_CALL(*loggerCopy, getDataLastTimestamp(::testing::_)).WillRepeatedly(::testing::Return(nullptr));
+      EXPECT_CALL(*loggerCopy, done(::testing::_, ::testing::_)).WillRepeatedly(::testing::Return());
+      EXPECT_CALL(*loggerCopy, start(::testing::_)).WillRepeatedly(::testing::Return(0));
+      EXPECT_CALL(*loggerCopy, isValid()).WillRepeatedly(::testing::Return(true));
+
+      auto logger = std::make_shared<terrama2::core::MockCollectorLogger>();
+
+      EXPECT_CALL(*logger, setConnectionInfo(::testing::_)).WillRepeatedly(::testing::Return());
+      EXPECT_CALL(*logger, setTableName(::testing::_)).WillRepeatedly(::testing::Return());
+      EXPECT_CALL(*logger, getLastProcessTimestamp(::testing::_)).WillRepeatedly(::testing::Return(nullptr));
+      EXPECT_CALL(*logger, getDataLastTimestamp(::testing::_)).WillRepeatedly(::testing::Return(nullptr));
+      EXPECT_CALL(*logger, done(::testing::_, ::testing::_)).WillRepeatedly(::testing::Return());
+      EXPECT_CALL(*logger, start(::testing::_)).WillRepeatedly(::testing::Return(0));
+      EXPECT_CALL(*logger, clone()).WillRepeatedly(::testing::Return(loggerCopy));
+      EXPECT_CALL(*logger, isValid()).WillRepeatedly(::testing::Return(true));
 
       terrama2::services::collector::core::Service service(dataManager);
-      auto logger = std::make_shared<terrama2::services::collector::core::CollectorLogger>();
-      logger->setConnectionInfo(uri);
+      serviceManager.setInstanceId(1);
+      serviceManager.setLogger(logger);
+      serviceManager.setLogConnectionInfo(te::core::URI(""));
+
       service.setLogger(logger);
       service.start();
 
-      terrama2::services::collector::core::Collector* collector(new terrama2::services::collector::core::Collector());
-      terrama2::services::collector::core::CollectorPtr collectorPtr(collector);
+      // DataProvider and DataSeries GradsFTP
+      auto dataProviderFTP = terrama2::gradsftp::dataProviderGradsFTP();
+      dataManager->add(dataProviderFTP);
+
+      auto inputDataSeriesFTP = terrama2::gradsftp::dataSeriesGradsFTP(dataProviderFTP);
+      dataManager->add(inputDataSeriesFTP);
+
+
+      // DataProvider and DataSeries GradsFILE
+      auto dataProviderFILE = terrama2::gradsftp::dataProviderGradsFILE();
+      dataManager->add(dataProviderFILE);
+
+      auto outputDataSeriesFILE = terrama2::gradsftp::dataSeriesGradsFILE(dataProviderFILE);
+      dataManager->add(outputDataSeriesFILE);
+
+
+      std::shared_ptr<terrama2::services::collector::core::Collector> collector = std::make_shared<terrama2::services::collector::core::Collector>();
+
       collector->id = 777;
-      collector->projectId = 1;
+      collector->projectId = 0;
       collector->serviceInstanceId = 1;
       collector->filter.region = terrama2::core::ewktToGeom("SRID=4326;POLYGON((-73.8036991603083 -9.81412714740936,-73.8036991603083 2.24662115728613,-56.097053202293 2.24662115728613,-56.097053202293 -9.81412714740936,-73.8036991603083 -9.81412714740936))");
       collector->filter.cropRaster = true;
-      collector->filter.discardBefore = terrama2::core::TimeUtils::stringToTimestamp("2016-11-25T06:00:00.000-02:00", terrama2::core::TimeUtils::webgui_timefacet);
-      collector->filter.discardAfter = terrama2::core::TimeUtils::stringToTimestamp("2016-11-25T12:00:00.000-02:00", terrama2::core::TimeUtils::webgui_timefacet);
+      collector->filter.discardBefore = terrama2::core::TimeUtils::stringToTimestamp("2017-10-10T12:00:00.000-02:00", terrama2::core::TimeUtils::webgui_timefacet);
+      collector->filter.discardAfter = terrama2::core::TimeUtils::stringToTimestamp("2017-10-12T10:30:00.000-02:00", terrama2::core::TimeUtils::webgui_timefacet);
 
-      collector->inputDataSeries = 1;
-      collector->outputDataSeries = 2;
-      collector->inputOutputMap.emplace(1, 2);
+      collector->inputDataSeries = inputDataSeriesFTP->id;
+      collector->outputDataSeries = outputDataSeriesFILE->id;
+      collector->inputOutputMap.emplace(inputDataSeriesFTP->id, outputDataSeriesFILE->id);
 
-      dataManager->add(collectorPtr);
+      dataManager->add(collector);
 
-      service.addToQueue(collectorPtr->id, terrama2::core::TimeUtils::nowUTC());
+      service.addToQueue(collector->id, terrama2::core::TimeUtils::nowUTC());
 
       QTimer timer;
       QObject::connect(&timer, SIGNAL(timeout()), QCoreApplication::instance(), SLOT(quit()));
