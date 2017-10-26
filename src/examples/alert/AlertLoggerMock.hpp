cf7274f6e1c387b5a291d1707598ce0d4eb5d6cf
BiancaMaciel <bianca_maciel.c@hotmail.com>
2017-10-26 18:25:02 -0200

    created google mock for alert

--- src/examples/alert/AlertLoggerMock.hpp
+++ src/examples/alert/AlertLoggerMock.hpp
@@ -15,6 +15,10 @@ class AlertLoggerMock : public terrama2::services::alert::core::AlertLogger
   public:
 
     MOCK_CONST_METHOD1(start, RegisterId(ProcessId processId));
+
+    MOCK_CONST_METHOD1(setTableName,
+                 void(std::string tableName));
+
     MOCK_CONST_METHOD3(result, void(ProcessLogger::Status status, const std::shared_ptr<te::dt::TimeInstantTZ> &dataTimestamp, RegisterId registerId));
     MOCK_CONST_METHOD1(getLastProcessTimestamp, std::shared_ptr< te::dt::TimeInstantTZ >(const ProcessId processId));
     MOCK_CONST_METHOD1(getDataLastTimestamp, std::shared_ptr< te::dt::TimeInstantTZ >(const ProcessId processId));
@@ -24,7 +28,10 @@ class AlertLoggerMock : public terrama2::services::alert::core::AlertLogger
     MOCK_CONST_METHOD1(internalClone, void(std::shared_ptr<terrama2::core::ProcessLogger> loggerCopy));
     MOCK_CONST_METHOD0(clone, std::shared_ptr<ProcessLogger>());
     MOCK_METHOD1(setConnectionInfo, void(const te::core::URI& uri));
+    MOCK_CONST_METHOD2(done,
+                       void(const std::shared_ptr< te::dt::TimeInstantTZ >& dataTimestamp, const RegisterId registerId));
 
+    MOCK_CONST_METHOD0(isValid, bool());
     void log(MessageType messageType, const std::string &description, RegisterId /*registerId*/) const override
     {
       std::cout << "Message Type: " << messageType << " Message: " << description << std::endl;
