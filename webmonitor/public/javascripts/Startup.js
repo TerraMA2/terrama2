c2c6cb4163656349af720895fe9bb6923145f427
Jean Souza <jeancfsouza@gmail.com>
2017-11-01 16:58:49 -0200

    starting translation

--- webmonitor/public/javascripts/Startup.js
+++ webmonitor/public/javascripts/Startup.js
@@ -40,6 +40,26 @@ requirejs(
     TerraMA2WebComponents.LayerExplorer.init();
     TerraMA2WebComponents.MapDisplay.init();
 
+
+
+
+    i18next.use(i18nextXHRBackend).init({
+      lng: 'en',
+      backend: {
+        loadPath: '/locales/{{lng}}.json'
+      }
+    }, function(err, t) {
+      // for options see
+      // https://github.com/i18next/jquery-i18next#initialize-the-plugin
+      jqueryI18next.init(i18next, $);
+      // start localizing, details:
+      // https://github.com/i18next/jquery-i18next#usage-of-selector-function
+      $('body').localize();
+    });
+
+
+
+
     Utils.init(null, function() {
       TerraMA2WebMonitor.init();
       Calendar.init();
