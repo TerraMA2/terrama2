ab1ed47756591eb7f4dd9290b2affb144ef73d52
Jano Simas <janosimas@gmail.com>
2017-11-08 08:57:12 -0200

    add comented varialbe names for better reading

--- src/terrama2/core/data-access/DataRetriever.hpp
+++ src/terrama2/core/data-access/DataRetriever.hpp
@@ -118,12 +118,12 @@ namespace terrama2
                                   std::function<void(const std::string& /*uri*/)> processFile) const;
 
         virtual void retrieveDataCallback(const std::string& /*mask*/,
-                                        const Filter& /*filter*/,
-                                        const std::string& /*timezone*/,
-                                        std::shared_ptr<terrama2::core::FileRemover> /*remover*/,
-                                        const std::string& /*temporaryFolderUri*/,
-                                        const std::string& /*foldersMask*/,
-                                        std::function<void(const std::string& /*uri*/, const std::string& /*filename*/)> /*processFile*/) const;
+                                          const Filter& /*filter*/,
+                                          const std::string& /*timezone*/,
+                                          std::shared_ptr<terrama2::core::FileRemover> /*remover*/,
+                                          const std::string& /*temporaryFolderUri*/,
+                                          const std::string& /*foldersMask*/,
+                                          std::function<void(const std::string& /*uri*/, const std::string& /*filename*/)> /*processFile*/) const;
 
         //! Returns the last data timestamp found on last access.
         virtual te::dt::TimeInstantTZ lastDateTime() const;
