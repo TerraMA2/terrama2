d949a7e668961627645d109a7e1de5ce4453fb6c
Jano Simas <janosimas@gmail.com>
2017-11-10 07:34:55 -0200

    fix data timestamp on error

--- src/terrama2/services/collector/core/Service.cpp
+++ src/terrama2/services/collector/core/Service.cpp
@@ -121,7 +121,7 @@ void terrama2::services::collector::core::Service::addToQueue(CollectorId collec
   catch(...)
   {
     // exception guard, slots should never emit exceptions.
-    TERRAMA2_LOG_ERROR() << QObject::tr("Unknown exception durring collector edd to queue...");
+    TERRAMA2_LOG_ERROR() << QObject::tr("Unknown exception during collector add to queue...");
   }
 }
 
@@ -186,6 +186,7 @@ void terrama2::services::collector::core::Service::collect(terrama2::core::Execu
     dataAccessor->getSeriesCallback(filter, remover, [&](const DataSetId& datasetId, const std::string& uri) {
       try
       {
+        std::shared_ptr<te::dt::TimeInstantTZ> lastDateTime;
         auto dataMap = dataAccessor->getSeries({{datasetId, uri}}, filter, remover);
         if(dataMap.empty())
         {
@@ -198,9 +199,7 @@ void terrama2::services::collector::core::Service::collect(terrama2::core::Execu
           sendProcessFinishedSignal(executionPackage.processId, executionPackage.executionDate, false);
           return;
         }
-        auto tempLastDateTime = dataAccessor->lastDateTime();
-        if(tempLastDateTime && (!lastDateTime || (*tempLastDateTime > *lastDateTime)))
-          lastDateTime = tempLastDateTime;
+        auto thisFileLastDateTime = dataAccessor->lastDateTime();
 
         /////////////////////////////////////////////////////////////////////////
         // data intersection
@@ -223,6 +222,10 @@ void terrama2::services::collector::core::Service::collect(terrama2::core::Execu
         auto dataStorager = terrama2::core::DataStoragerFactory::getInstance().make(outputDataSeries, outputDataProvider);
 
         dataStorager->store(dataMap, dataSetLst, inputOutputMap);
+
+        // if any exception happens, don't update the data timestamp
+        if(thisFileLastDateTime && (!lastDateTime || (*thisFileLastDateTime > *lastDateTime)))
+          lastDateTime = thisFileLastDateTime;
       }
       catch(const terrama2::core::LogException& e)
       {
