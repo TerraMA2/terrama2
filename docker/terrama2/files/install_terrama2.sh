10032f7e23fa630234c05e10c46c07f8879692aa
raphaelrpl <raphael.wcosta@gmail.com>
2017-11-13 13:54:26 -0200

    Update terrama2 rc4 and Port expose in docker-compose

--- docker/terrama2/files/install_terrama2.sh
+++ docker/terrama2/files/install_terrama2.sh
@@ -12,11 +12,11 @@ function valid()
 
 # TODO: Change URL to http://www.dpi.inpe.br/terrama2/doku.php?id=download and change curl parameter to -o deb_package_name
 echo -ne "Downloading TerraMA² ... "
-curl -O http://www.dpi.inpe.br/jenkins-data/terrama2/installers/linux/TerraMA2-4.0.0-rc3-linux-x64-Ubuntu-14.04.deb --silent
+curl -O http://www.dpi.inpe.br/jenkins-data/terrama2/installers/linux/TerraMA2-4.0.0-rc4-linux-x64-Ubuntu-14.04.deb --silent
 valid $? "Error: Could not fetch TerraMA²"
 
 echo "Installing TerraMA² ... "
-dpkg -i TerraMA2-4.0.0-rc3-linux-x64-Ubuntu-14.04.deb 2> /dev/null
+dpkg -i TerraMA2-4.0.0-rc4-linux-x64-Ubuntu-14.04.deb 2> /dev/null
 
 apt-get install -f -y
 valid $? "Error: Could not install TerraMA²"
