3dd1a8a3f7f445060ecc43980937856fb82aee9d
Jano Simas <janosimas@gmail.com>
2017-11-17 08:36:57 -0200

    using utils create rtree method at intersection

--- src/terrama2/services/collector/core/IntersectionOperation.cpp
+++ src/terrama2/services/collector/core/IntersectionOperation.cpp
@@ -37,6 +37,7 @@
 #include "../../../core/utility/DataAccessorFactory.hpp"
 #include "../../../core/utility/Logger.hpp"
 #include "../../../core/utility/Utils.hpp"
+#include "../../../core/utility/GeoUtils.hpp"
 #include "../../../core/utility/Verify.hpp"
 #include "../../../core/data-access/DataAccessor.hpp"
 #include "../../../core/data-access/DataAccessorGrid.hpp"
@@ -151,11 +152,11 @@ terrama2::core::DataSetSeries terrama2::services::collector::core::processVector
 
   auto remover = std::make_shared<terrama2::core::FileRemover>();
   // Reads data
-  auto seriesMap = accessor->getSeries(filter, remover);
+  auto intersectionSeriesMap = accessor->getSeries(filter, remover);
 
   std::map<std::string, std::string> mapAlias;
 
-  for(auto it = seriesMap.begin(); it != seriesMap.end(); ++it)
+  for(auto it = intersectionSeriesMap.begin(); it != intersectionSeriesMap.end(); ++it)
   {
     auto interDsType = it->second.teDataSetType;
     auto interDs = it->second.syncDataSet;
@@ -187,8 +188,8 @@ terrama2::core::DataSetSeries terrama2::services::collector::core::processVector
 
     }
 
-    te::sam::rtree::Index<size_t, 8>* rtree(new te::sam::rtree::Index<size_t, 8>);
-
+    // Creates a rtree with all occurrences
+    auto rtree = terrama2::core::createRTreeFromSeries(collectedDataSetSeries);
 
     auto collectedGeomProperty = te::da::GetFirstGeomProperty(collectedDataSetType.get());
     if(!collectedGeomProperty)
@@ -215,16 +216,14 @@ terrama2::core::DataSetSeries terrama2::services::collector::core::processVector
     outputDt.reset(createDataSetType(collectedDataSetType.get(), interProperties));
     outputDs.reset(new te::mem::DataSet(outputDt.get()));
 
-    // Creates a rtree with all occurrences
     for(unsigned int i = 0; i < collectedData->size(); ++i)
     {
       auto geometry = collectedData->getGeometry(i, collectedGeomPropertyPos);
-      rtree->insert(*geometry->getMBR(), i);
 
-      te::mem::DataSetItem* item = new te::mem::DataSetItem(outputDs.get());
+      std::unique_ptr<te::mem::DataSetItem> item(new te::mem::DataSetItem(outputDs.get()));
 
-      te::gm::Geometry* occurrenceGeom(dynamic_cast<te::gm::Geometry*>(geometry->clone()));
-      item->setGeometry(collectedGeomProperty->getName(), occurrenceGeom);
+      std::unique_ptr<te::gm::Geometry> occurrenceGeom(dynamic_cast<te::gm::Geometry*>(geometry->clone()));
+      item->setGeometry(collectedGeomProperty->getName(), occurrenceGeom.release());
 
       // copies all attributes from the collected dataset
       for(size_t j = 0; j < collectedProperties.size(); ++j)
@@ -242,11 +241,8 @@ terrama2::core::DataSetSeries terrama2::services::collector::core::processVector
         }
       }
 
-      outputDs->add(item);
-
+      outputDs->add(item.release());
     }
-
-
 
     for(unsigned int i = 0; i < interDs->size(); ++i)
     {
