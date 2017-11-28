61a3053e9b789f178d4cb68cbcc3a3447bfa936d
BiancaMaciel <bianca_maciel.c@hotmail.com>
2017-11-28 16:34:15 -0200

    revised scripts

--- share/terrama2/scripts/compare-analysis.py
+++ share/terrama2/scripts/compare-analysis.py
@@ -2,32 +2,26 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-tablesName = ["analysis_dcp_result"]
+
+tableName = "analysis_dcp_result"
 
 def compareQuery(tableName):
-    return "SELECT * FROM "+tableName+"_ref WHERE not EXISTS (SELECT * FROM "+tableName+" WHERE "+tableName+".* = "+tableName+"_ref.* )"
+    #return "SELECT * FROM "+tableName+"_ref WHERE not EXISTS (SELECT * FROM "+tableName+" WHERE "+tableName+".* = "+tableName+"_ref.* )"
+     return "SELECT result.*, reference.* FROM "+tableName+" result INNER JOIN "+tableName+"_ref reference ON (result.id = reference.id) WHERE not EXISTS (SELECT * FROM "+tableName+"_ref WHERE result.execution_date = reference.execution_date and result.max = reference.max)"
+
+
+
+
 
 def checkTable(tableName):
-    conn = psycopg2.connect("dbname='test' user='postgres' password='postgres' host = '127.0.0.1' port= '5432' ")
+    conn = psycopg2.connect("dbname='"+dbname+"' user='postgres' password='postgres' host = '127.0.0.1' port= '5432' ")
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = conn.cursor()
     cur.execute(compareQuery(tableName))
     rows = cur.fetchall()
-    st = status_compare(tableName, rows)
-    return st
-
-
-def status_compare(tableName, rows):
     if rows:
-        print("Failed test Analysis  " + tableName)
-        status = "Failed"
-        for row in rows:
-            print(row);
+	return 0
     else:
-        print("Passed test Analysis  " + tableName)
-        status = "Passed"
-    return status
+	return 1
 
-
-for tableName in tablesName:
-    st = checkTable(tableName)
+status = checkTable(tableName)
--- share/terrama2/scripts/compare-collector.py
+++ share/terrama2/scripts/compare-collector.py
@@ -7,31 +7,23 @@ def compareQuery(tableName):
     return "SELECT * FROM "+tableName+"_ref WHERE not EXISTS (SELECT * FROM "+tableName+" WHERE "+tableName+".* = "+tableName+"_ref.* )"
 
 def checkTable(tableName):
-    conn = psycopg2.connect("dbname='test' user='postgres' password='postgres' host = '127.0.0.1' port= '5432' ")
+    conn = psycopg2.connect("dbname='"+dbname+"' user='postgres' password='postgres' host = '127.0.0.1' port= '5432' ")
     conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     cur = conn.cursor()
     cur.execute(compareQuery(tableName))
     rows = cur.fetchall()
-    st = status_compare(tableName, rows)
-    return st
-
-
-def status_compare(tableName, rows):
     if rows:
-        print("Failed Test Collector " + tableName)
-        status = "Failed"
-        for row in rows:
-            print(row);
+        status = 0       
     else:
-        print("Passed Test Collector " + tableName)
-        status = "Passed"
+        status = 1
     return status
 
-cont = 0
+
+count = 0
 for tableName in tablesName:
     st = checkTable(tableName)
-    if st == "Passed":
-        cont = cont + 1
+    if st == 1:
+        count = count + 1
 
 
 
