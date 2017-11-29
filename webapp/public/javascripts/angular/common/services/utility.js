83ee7439883728e23462db50a2c9b6f7efc033ea
franciscovn <franciscovn909@gmail.com>
2017-11-29 13:28:06 -0200

    Fix rbga button

--- webapp/public/javascripts/angular/common/services/utility.js
+++ webapp/public/javascripts/angular/common/services/utility.js
@@ -21,7 +21,7 @@ define([], function() {
     }
 
     function rgba2hex(r, g, b, a) {
-      return "#" + (r | 1 << 8).toString(16).slice(1) + (g | 1 << 8).toString(16).slice(1) + (b | 1 << 8).toString(16).slice(1) + ((Math.round(255 * a)) | 1 << 8).toString(16).slice(1);
+      return "#" + (r | 1 << 8).toString(16).slice(1) + (g | 1 << 8).toString(16).slice(1) + (b | 1 << 8).toString(16).slice(1) + (a | 1 << 8).toString(16).slice(1);
     }
 
     function hex2rgba(hexColor){
