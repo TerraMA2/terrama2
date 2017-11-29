fde8a28a53c78ff50a678585c448f16569831fb4
franciscovn <franciscovn909@gmail.com>
2017-11-29 13:58:27 -0200

    Show auto legend button on change event

--- webapp/public/javascripts/angular/views/components/style.js
+++ webapp/public/javascripts/angular/views/components/style.js
@@ -251,6 +251,19 @@ define([], function () {
      * @returns {void}
      */
     self.getColumnValues = function(){
+      if(self.columnsList.length == 0){
+        self.showAutoCreateLegendButton = false;
+        return;
+      } else {
+        var hasInList = self.columnsList.some(function(column){
+          return column == self.model.metadata.attribute;
+        });
+        if (!hasInList){
+          self.showAutoCreateLegendButton = false;
+          return;
+        }
+
+      }
       if (self.model.type == 3 && self.model.metadata.attribute !== undefined && self.model.metadata.attribute !== ""){
         DataProviderService.listPostgisObjects({providerId: self.postgisData.dataProvider.id, objectToGet: "values", tableName: self.postgisData.tableName, columnName: self.model.metadata.attribute})
           .then(function(response){
