8053ab09bd60ef65a9f52da0b160c5440e828f93
BiancaMaciel <bianca_maciel.c@hotmail.com>
2017-12-20 14:56:37 -0200

    modificated scripts python

--- share/terrama2/scripts/compare-analysis.py
+++ share/terrama2/scripts/compare-analysis.py
@@ -7,6 +7,7 @@ def checkTable(query):
     cur = conn.cursor()
     cur.execute(query)
     rows = cur.fetchall()
+    cur.close()
     if rows:
 	return 0
     else:
@@ -14,12 +15,20 @@ def checkTable(query):
 
 
 typeAn = typeAnalysis
-if(typeAn == "dcp_history" or  typeAn == "operator_dcp" or  typeAn == "operator_history_interval"):
+if(typeAn == "dcp_history" or  typeAn == "operator_history_interval"):
     tableName = "analysis_dcp_result"
-    query = "SELECT result.*, reference.* FROM "+tableName+" result INNER JOIN "+tableName+"_ref reference ON (result.id = reference.id) WHERE NOT EXISTS (SELECT * FROM "+tableName+"_ref WHERE result.execution_date = reference.execution_date and result.max = reference.max and result.count = reference.count and result.mean = reference.mean and result.median = reference.median and result.min = reference.min and result.sum = reference.sum and result.value = reference.value and result.standard_deviation = reference.standard_deviation and result.variance = reference.variance)"
+    query = "SELECT result.*, reference.* FROM "+tableName+"  result INNER JOIN "+tableName+"_ref reference ON (result.id = reference.id) WHERE NOT EXISTS (SELECT * FROM analysis_dcp_result_ref WHERE result.id = reference.id AND result.execution_date = reference.execution_date AND result.max = reference.max AND result.count = reference.count AND result.mean = reference.mean AND result.median = reference.median AND result.min = reference.min AND result.sum = reference.sum AND result.value = reference.value AND result.standard_deviation = reference.standard_deviation AND result.variance = reference.variance)"
+    status = checkTable(query)
+elif(typeAn == "operator_dcp"):
+    tableName = "analysis_dcp_result"
+    query = "SELECT result.*, reference.* FROM "+tableName+"  result INNER JOIN "+tableName+"_ref reference ON (result.id = reference.id) WHERE NOT EXISTS (SELECT * FROM analysis_dcp_result_ref WHERE result.id = reference.id AND result.execution_date = reference.execution_date AND result.max = reference.max AND result.count = reference.count AND result.mean = reference.mean AND result.median = reference.median AND result.min = reference.min AND result.sum = reference.sum AND result.value = reference.value)"
     status = checkTable(query)
 elif (typeAn == "occ"):
     tableName = "occurrence_analysis_result"
     #query = "SELECT result.*, reference.* FROM "+tableName+" result INNER JOIN "+tableName+"_ref reference ON (result.fid = reference.fid) WHERE NOT EXISTS (SELECT * FROM "+tableName+"_ref WHERE result.execution_date = reference.execution_date and result.count = reference.count)"
     query = "SELECT execution_date, count FROM "+tableName+"_ref WHERE NOT EXISTS (SELECT * FROM "+tableName+" WHERE "+tableName+".execution_date = "+tableName+"_ref.execution_date and "+tableName+".count = "+tableName+"_ref.count )"
     status = checkTable(query)
+
+
+
+
--- share/terrama2/scripts/delete-db.py
+++ share/terrama2/scripts/delete-db.py
@@ -6,3 +6,4 @@ conn = psycopg2.connect("user='postgres' password='postgres' host = '127.0.0.1' 
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = conn.cursor()
 cur.execute('DROP DATABASE IF EXISTS '  + dbname)
+cur.close()
