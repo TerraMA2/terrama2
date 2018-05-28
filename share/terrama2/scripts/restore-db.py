e6d1eb5dcef838821bbb4e87ab29de33435d94a9
Jano Simas <janosimas@gmail.com>
2018-05-28 11:03:49 -0300

    don't exibit pg_restore messages in tests

--- share/terrama2/scripts/restore-db.py
+++ share/terrama2/scripts/restore-db.py
@@ -1,12 +1,13 @@
 import psycopg2
-import os, sys
+import os
+import subprocess
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 conn = psycopg2.connect("dbname='"+dbname+"' user='postgres' password='postgres' host = '127.0.0.1' port= '5432' ")
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 cur = conn.cursor()
 
-command = "/usr/bin/pg_restore -h 'localhost' -p '5432' -U 'postgres' -d 'test' -v '"+namefile+"' "
-os.system(command)
+command = ["/usr/bin/pg_restore", "-h", "localhost", "-p", "5432", "-U", "postgres", "-d", "test", "-v", namefile]
+process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 cur.close()
 
