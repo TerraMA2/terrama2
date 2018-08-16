d96d6885dc6b084eb814b437ec6880dd623ebcfd
Jano Simas <janosimas@gmail.com>
2018-08-16 13:26:08 -0300

    Improved check for number of available threads

--- src/terrama2/services/analysis/core/Service.cpp
+++ src/terrama2/services/analysis/core/Service.cpp
@@ -137,6 +137,7 @@ void terrama2::services::analysis::core::Service::start(size_t threadNumber)
 {
   terrama2::core::Service::start(threadNumber);
   threadNumber = std::ceil(verifyNumberOfThreads(threadNumber)/2);
+  if(threadNumber < 1) threadNumber = 1;
   threadPool_.reset(new ThreadPool(threadNumber));
 }
 
