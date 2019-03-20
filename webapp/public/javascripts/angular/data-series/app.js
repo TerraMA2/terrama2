ec55c0973b484f9cd23104958bfa4f8ab6aaca4c
Marcelo Pilatti <marcelo.silva979@gmail.com>
2019-03-20 10:33:14 -0300

    Added responsive classes on all tables

--- webapp/public/javascripts/angular/data-series/app.js
+++ webapp/public/javascripts/angular/data-series/app.js
@@ -5,13 +5,11 @@ define([
   "TerraMA2WebApp/data-series/schedule",
   "TerraMA2WebApp/data-series/components/index",
   "TerraMA2WebApp/data-series/directives"
-], function(moduleLoader, controllersApp, servicesApp, scheduleApp, componentsApp, directives) {
+], function(moduleLoader, controllersApp, servicesApp, scheduleApp, componentsApp, directives, attributes) {
   var moduleName = "terrama2.dataseries";
 
   var deps = [controllersApp, servicesApp, scheduleApp, componentsApp, directives];
   moduleLoader("ngFileUpload", deps);
-
-  angular.module(moduleName, deps);
 
   return moduleName;
 });
