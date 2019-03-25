521926d6b0b9cd6251fd2237952159de85a14f2c
raphaelrpl <raphael.wcosta@gmail.com>
2019-03-25 09:22:12 -0300

    Update unittest for ShapeImporter

--- webapp/tests/core/ShapeImporter.spec.js
+++ webapp/tests/core/ShapeImporter.spec.js
@@ -1,8 +1,10 @@
 const expect = require('chai').expect;
 const sinon = require('sinon');
+const fs = require('fs');
 const child_process = require('child_process');
 
 // TerraMA2
+const DataManager = require('./../../core/DataManager');
 const FileExtract = require('./../../core/FileExtract');
 const Exportation = require('./../../core/Exportation');
 const { MultipleShapeFileFoundError, NoShapeFileFoundError, ShapeImporter } = require('./../../core/ShapeImporter');
@@ -122,5 +124,85 @@ describe('ShapeImporter test', () => {
     });
 
   });
+
+  describe('#toDataProvider', () => {
+    let unzipStub = null;
+    let nodeChildProcessStud = null;
+
+    const successDataProviderStub = () => (
+      sinon.stub(DataManager, 'getDataProvider').resolves({ id: 1, name: 'Fake DataProvider', uri: 'file:///fakePath' })
+    )
+
+    beforeEach(() => {
+      unzipStub = sinon.stub(FileExtract, 'unzip').returns({
+        getEntries: () => [{ entryName: `${temporaryDir}/fake.shp` }]
+      });
+
+      importer.unzip('/fake/path');
+      expect(importer.getFiles()).to.have.length(1);
+
+      nodeChildProcessStud = sinon.stub(child_process, 'exec').yields(null, 'fake');
+    });
+
+    afterEach(() => {
+      unzipStub.restore();
+      nodeChildProcessStud.restore();
+    });
+
+    it('exports shapefile to FILE data provider', async () => {
+      const dataManagerStub = successDataProviderStub();
+      const fsStub = sinon.stub(fs, 'existsSync').onCall(0).returns(true).onCall(1).returns(false);
+      const exportationStub = sinon.stub(Exportation.prototype, 'copyShpFiles').returns(undefined);
+
+      const undefinedResult = await importer.toDataProvider(1, 'fakeShape.shp');
+
+      exportationStub.restore();
+      fsStub.restore();
+      dataManagerStub.restore();
+
+      expect(undefinedResult).to.be.undefined;
+    });
+
+    it('throws error when data provider not found', async () => {
+      const dataManagerStub = sinon.stub(DataManager, 'getDataProvider').rejects(new Error('Not found'));
+
+      try {
+        await importer.toDataProvider(1, 'fakeShape.shp');
+      } catch (err) {
+        expect(err.message).to.be.equal('Not found');
+      } finally {
+        dataManagerStub.restore();
+      }
+    });
+
+    it('throws error when there is invalid path in data provider', async () => {
+      const dataManagerStub = successDataProviderStub();
+      const fsStub = sinon.stub(fs, 'existsSync').returns(false);
+
+      try {
+        await importer.toDataProvider(1, 'fakeShape.shp');
+      } catch (err) {
+        expect(err.message).to.be.equal('Invalid data provider path!');
+      }
+
+      fsStub.restore();
+      dataManagerStub.restore();
+    });
+
+    it('throws error when shapefile already exists', async () => {
+      const dataManagerStub = successDataProviderStub();
+      const fsStub = sinon.stub(fs, 'existsSync').onCall(0).returns(true).onCall(1).returns(true);
+
+      try {
+        await importer.toDataProvider(1, 'fakeShape.shp');
+      } catch (err) {
+        expect(err.message).to.be.equal('File already exists!');
+      }
+
+      fsStub.restore();
+      dataManagerStub.restore();
+    });
+  });
+
 
 });
