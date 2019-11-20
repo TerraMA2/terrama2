26a0d870bd1372f858492dd52763a4f77911b1c0
miguel <miguelthiago.ufla@gmail.com>
2019-11-20 08:48:20 -0300

    fixed seach static data

--- webapp/core/facade/ReportService.js
+++ webapp/core/facade/ReportService.js
@@ -1,8 +1,6 @@
 (function() {
   'use strict';
 
-  const DataManager = require('../../core/DataManager');
-  const ViewFacade = require("../../core/facade/View");
   const Filter = require("./Filter");
   const {Connection} = require('../../core/utility/connection');
 
@@ -17,7 +15,7 @@
 
     const tableName = await Filter.getTableName(conn, view.id);
 
-    const filter =  await Filter.setWhere(conn, params);
+    const filter =  await Filter.setFilter(conn, params);
 
     let sqlWhere = filter.sqlWhere;
 
@@ -45,7 +43,7 @@
       result = await conn.execute(sql);
       let dataJson = result.rows;
 
-      if (count) {
+      if (params.count) {
         const sqlCount =
             ` SELECT COUNT(*) AS count FROM public.${tableName} AS main_table
               ${filter.secondaryTables}
