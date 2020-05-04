3bb9e92695a93e8d4d45737e06734b7cd1d70af7
juan.goncalves <juan.goncalves@inpe.br>
2020-05-04 15:20:09 -0300

    Change arrows to show in outlook - alert email

--- src/terrama2/services/alert/core/Utils.cpp
+++ src/terrama2/services/alert/core/Utils.cpp
@@ -107,7 +107,7 @@ std::string terrama2::services::alert::core::dataSetHtmlTable(const std::shared_
 
   htmlTable += "</table>";
 
-  htmlTable +="<br />* Comparação entre as duas últimas análises ( = :sem alteração, 🡅 :aumentou, 🡇 :diminuiu e 🚫:houve algum erro no dado)<br />";
+  htmlTable +="<br />* Comparação entre as duas últimas análises ( = :sem alteração, ↥ :aumentou, ↧ :diminuiu e 🚫:houve algum erro no dado)<br />";
 
   return htmlTable;
 }
