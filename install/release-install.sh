ed2d2865ce1e75320c2677a2c8c8e29a1fbdd5c3
marcelopilatti <marcelo.silva979@gmail.com>
2020-06-12 09:58:38 -0300

    Added tooltip with view description on the layers

--- install/release-install.sh
+++ install/release-install.sh
@@ -15,7 +15,7 @@ ssh-keygen -t rsa -b 4096 -C "terrama2-team@dpi.inpe.br" -N "" -f $HOME/.ssh/id_
 
 mkdir terralib-installer
 cd terralib-installer
-if [ ! test -f "terralib-5.4.5-ubuntu-16.04_2019-12-13_15-45-5.4.5.tar.gz" ]
+if ! [ test -f "terralib-5.4.5-ubuntu-16.04_2019-12-13_15-45-5.4.5.tar.gz" ]
 then
     wget -q http://www.dpi.inpe.br/jenkins-data/terralib/installers/linux/terralib-5.4.5-ubuntu-16.04_2019-12-13_15-45-5.4.5.tar.gz
 fi
@@ -29,14 +29,14 @@ mkdir terrama2-deb
 
 cd terrama2-deb
 
-if [ ! test -f "TerraMA2-4.1.0-release-linux-x64-Ubuntu-16.04.deb" ]
+if ! [ test -f "TerraMA2-4.1.0-release-linux-x64-Ubuntu-16.04.deb" ]
 then
     wget -q http://www.dpi.inpe.br/jenkins-data/terrama2/installers/linux/final/TerraMA2-4.1.0-release-linux-x64-Ubuntu-16.04.deb
 fi
 
 sudo dpkg -i TerraMA2-4.1.0-release-linux-x64-Ubuntu-16.04.deb
 
-if [ ! test -f "terrama2-doc-4.1.0.deb" ]
+if ! [ test -f "terrama2-doc-4.1.0.deb" ]
 then
     wget -q http://www.dpi.inpe.br/jenkins-data/terrama2/installers/linux/final/terrama2-doc-4.1.0.deb
 fi
