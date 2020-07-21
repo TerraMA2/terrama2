7032e2bece290fa1b8acc53fa221562d57e4e7e0
Juan Vinicius <juan_vinicius_19@yahoo.com.br>
2020-07-21 16:09:18 -0300

    Revert "B4.1.0"

--- webapp/public/javascripts/angular/analysis/controllers/register-update.js
+++ webapp/public/javascripts/angular/analysis/controllers/register-update.js
@@ -30,11 +30,6 @@ define([], function() {
 
     self.columnsList = [];
 
-    $scope.$watch("model.table_name", function(val) {
-
-      console.log(val);
-    });
-
     var hasProjectPermission = config.hasProjectPermission;
 
     // Flag to verify if can not save if the service is not running
