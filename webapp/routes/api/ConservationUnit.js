92d1c39efb7c6cc9b46b4c5fe8c77bb168730169
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-07-24 17:21:45 -0300

    Merge

--- webapp/routes/api/ConservationUnit.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-    'use strict';
-
-    var controller = app.controllers.api.ConservationUnit;
-
-    app.get(app.locals.BASE_URL + 'api/conservationUnit/getAll', controller.getAll);
-};
--- webapp/routes/api/Projus.js
+++
@@ -1,7 +0,0 @@
-module.exports = function (app) {
-    'use strict';
-
-    var controller = app.controllers.api.Projus;
-
-    app.get(app.locals.BASE_URL + 'api/projus/getAll', controller.getAll);
-};
