d6ee72dee9804155e11670bfafcc0b945fc2038e
Marcelo Pilatti <marcelo.silva979@gmail.com>
2020-07-24 17:36:19 -0300

    Removed unused files

--- webapp/routes/api/Car.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (app) {
-    'use strict';
-
-    var controller = app.controllers.api.Car;
-
-    app.get(app.locals.BASE_URL + 'api/car/getAllSimplified', controller.getAllSimplified);
-    app.get(app.locals.BASE_URL + 'api/car/getAll', controller.getAll);
-    app.get(app.locals.BASE_URL + 'api/car/getByCpf', controller.getByCpf);
-};
